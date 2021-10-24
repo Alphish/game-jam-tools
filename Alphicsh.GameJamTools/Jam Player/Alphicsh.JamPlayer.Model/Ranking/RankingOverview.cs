b6ac4c2ca12b904e31b037314b778cc02af9f2ab
Alice Jankowska <alphish.creature@gmail.com>
2021-10-24 15:27:26 +0200

    [Feature] Created a model for RankingOverview

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Ranking/RankingOverview.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Model.Ranking
+{
+    public sealed class RankingOverview
+    {
+        public IList<RankingEntry> RankedEntries { get; set; }
+        public IList<RankingEntry> UnrankedEntries { get; set; }
+
+        public RankingOverview()
+        {
+            RankedEntries = new List<RankingEntry>();
+            UnrankedEntries = new List<RankingEntry>();
+        }
+    }
+}
