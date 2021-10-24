505300eee9aaabd40429bef8a48292a7eab22760
Alice Jankowska <alphish.creature@gmail.com>
2021-10-24 10:03:39 +0200

    [Feature] Created a user control for ranking list entry

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Ranking/RankingListEntry.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.Ranking
+{
+    /// <summary>
+    /// Interaction logic for RankingListEntry.xaml
+    /// </summary>
+    public partial class RankingListEntry : UserControl
+    {
+        public RankingListEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
