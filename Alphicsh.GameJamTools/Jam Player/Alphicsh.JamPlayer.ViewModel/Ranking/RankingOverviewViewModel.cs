0babcd42956a767d86df9c5ba3a53867bcbbde6d
Alice Jankowska <alphish.creature@gmail.com>
2021-10-24 15:27:42 +0200

    [Feature] Created a ViewModel for RankingOverview

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Ranking/RankingOverviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Alphicsh.JamPlayer.Model.Ranking;
+
+namespace Alphicsh.JamPlayer.ViewModel.Ranking
+{
+    public class RankingOverviewViewModel : BaseViewModel<RankingOverview>
+    {
+        public RankingOverviewViewModel(RankingOverview model)
+            : base(model)
+        {
+            RankedEntries = CollectionViewModel.Create(model.RankedEntries, entry => new RankingEntryViewModel(entry));
+            UnrankedEntries = CollectionViewModel.Create(model.UnrankedEntries, entry => new RankingEntryViewModel(entry));
+        }
+
+        public CollectionViewModel<RankingEntry, RankingEntryViewModel> RankedEntries { get; }
+        public CollectionViewModel<RankingEntry, RankingEntryViewModel> UnrankedEntries { get; }
+    }
+}
