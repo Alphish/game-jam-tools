210a74f500c2fbebf5be01b676be453303778f52
Alice Jankowska <alphish.creature@gmail.com>
2021-10-24 15:29:28 +0200

    [Feature] Created a user control for ranking list

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Ranking/RankingList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Alphicsh.JamPlayer.Controls.Ranking
+{
+    /// <summary>
+    /// Interaction logic for RankingList.xaml
+    /// </summary>
+    public partial class RankingList : UserControl
+    {
+        public RankingList()
+        {
+            InitializeComponent();
+        }
+    }
+}
