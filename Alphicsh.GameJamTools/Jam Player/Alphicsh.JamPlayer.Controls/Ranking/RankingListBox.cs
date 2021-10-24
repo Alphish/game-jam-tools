3443d908dacf016bf8834a83be3a9ed67302d8b1
Alice Jankowska <alphish.creature@gmail.com>
2021-10-25 00:23:21 +0200

    [Feature] Created a custom RankingListBox control
    In particular, this control ensures that after an element is dropped onto it, it'll reload both the unranked and ranked entries.

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Ranking/RankingListBox.cs
@@ -0,0 +1,25 @@
+﻿using Alphicsh.JamTools.Common.Controls;
+
+using Alphicsh.JamPlayer.ViewModel.Ranking;
+using System.Windows;
+
+namespace Alphicsh.JamPlayer.Controls.Ranking
+{
+    public class RankingListBox : ReorderableListBox<RankingEntryViewModel>
+    {
+        public static readonly DependencyProperty RankingOverviewProperty
+            = DependencyProperty.Register(nameof(RankingOverview), typeof(RankingOverviewViewModel), typeof(RankingListBox));
+
+        public RankingOverviewViewModel RankingOverview
+        {
+            get => (RankingOverviewViewModel)GetValue(RankingOverviewProperty);
+            set => SetValue(RankingOverviewProperty, value);
+        }
+
+        public override void AfterDrop()
+        {
+            RankingOverview.RankedEntries.CompleteChanges();
+            RankingOverview.UnrankedEntries.CompleteChanges();
+        }
+    }
+}
