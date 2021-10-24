078a19f9a0ee25abe9cc34b5616f8942d7cf5409
Alice Jankowska <alphish.creature@gmail.com>
2021-10-24 10:01:18 +0200

    [Feature] Created a simple BaseViewModel class
    It doesn't yet have a NotifyPropertyChanged implementation, but it will in due time.

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Alphicsh.JamPlayer.ViewModel
+{
+    public abstract class BaseViewModel<TModel>
+    {
+        protected TModel Model { get; }
+
+        protected BaseViewModel(TModel model)
+        {
+            Model = model;
+        }
+    }
+}
