1521c041f1b8dfc05af19470308bf9c964dc350a
Alice Jankowska <alphish.creature@gmail.com>
2021-10-24 10:02:20 +0200

    [Feature] Created models for Jam entry information and the ranking entry wrapping it

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Jam/JamEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace Alphicsh.JamPlayer.Model.Jam
+{
+    public sealed class JamEntry
+    {
+        public string Title { get; init; } = null!;
+        public JamTeam Team { get; init; } = null!;
+    }
+}
