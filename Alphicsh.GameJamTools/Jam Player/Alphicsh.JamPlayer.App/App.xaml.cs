1218d7d6631f2f16754662ec88d048c4c560dc6b
Alice Jankowska <alphish.creature@gmail.com>
2021-10-27 23:16:05 +0200

    [Dummy] Populated App jam information with dummy data

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.App/App.xaml.cs
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.App/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Media.Imaging;
 
 using Alphicsh.JamPlayer.Model;
+using Alphicsh.JamPlayer.Model.Jam;
 using Alphicsh.JamPlayer.ViewModel;
 
 namespace Alphicsh.JamPlayer.App
@@ -23,6 +26,8 @@ namespace Alphicsh.JamPlayer.App
         {
             LoadImageSourceResource("EntryPlaceholderSource", "Alphicsh.JamPlayer.App.Content.entry_placeholder.png");
 
+            InitData();
+
             base.OnStartup(e);
         }
 
@@ -39,6 +44,33 @@ namespace Alphicsh.JamPlayer.App
             imageSource.EndInit();
 
             Resources[resourceKey] = imageSource;
+        }
+
+        private void InitData()
+        {
+            var jamModel = new JamOverview()
+            {
+                Entries = new List<JamEntry>
+                {
+                    CreateJamEntry("Another Winning Entry", teamName: null, "TehPilot", "TheFugue"),
+                    CreateJamEntry("Cthildha", teamName: null, "Jordan Thomas", "Amber Thomas"),
+                    CreateJamEntry("Escape of the Clowns", "Firehammer Games", "kburkhart84"),
+                    CreateJamEntry("Evanski's Raccoon Adventure", teamName: null, "EvanSki"),
+                    CreateJamEntry("Forty; or the Modern Big Brother", teamName: null, "Ezra"),
+                    CreateJamEntry("Mansion Mayhem", "Team 70s", "Alice", "HayManMarc", "Micah_DS"),
+                    CreateJamEntry("The Insufferable Pan", teamName: "Pixel-Team", "Pat Ferguson"),
+                    CreateJamEntry("Ullr", teamName: null, "Siolfor the Jackal"),
+                }
+            };
+
+            ViewModel.LoadJam(jamModel);
+        }
+
+        private JamEntry CreateJamEntry(string title, string? teamName, params string[] authorNames)
+        {
+            var authors = authorNames.Select(name => new JamAuthor { Name = name }).ToList();
+            var team = new JamTeam { Name = teamName, Authors = authors };
+            return new JamEntry { Title = title, Team = team };
         }
     }
 }
