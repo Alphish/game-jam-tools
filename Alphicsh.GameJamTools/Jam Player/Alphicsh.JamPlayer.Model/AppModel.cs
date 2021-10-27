fe25d09bfd2c0373aad070a473066e8e56d9f742
Alice Jankowska <alphish.creature@gmail.com>
2021-10-27 23:10:18 +0200

    [Feature] Created models for the jam overview and for the application itself

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/AppModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Alphicsh.JamPlayer.Model.Jam;
+using Alphicsh.JamPlayer.Model.Ranking;
+
+namespace Alphicsh.JamPlayer.Model
+{
+    public class AppModel
+    {
+        public JamOverview Jam { get; private set; }
+        public RankingOverview Ranking { get; private set; }
+
+        public AppModel()
+        {
+            Jam = new JamOverview { Entries = new List<JamEntry>() };
+            Ranking = new RankingOverview();
+        }
+
+        public void LoadJam(JamOverview jam)
+        {
+            Jam = jam;
+            Ranking = new RankingOverview();
+
+            // TODO: add pending entries to randomly pick the next entry from
+            Ranking.UnrankedEntries = jam.Entries
+                .Select(entry => new RankingEntry { JamEntry = entry })
+                .ToList();
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Jam/JamOverview.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Model.Jam
+{
+    public class JamOverview
+    {
+        public IReadOnlyCollection<JamEntry> Entries { get; init; } = default!;
+    }
+}
