0ea7cd4d9f4933bd753041c858fb026a31845a5b
Alice Jankowska <alphish.creature@gmail.com>
2021-10-31 19:48:46 +0100

    [Feature] Added a control for Jam entry summary

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/JamEntry/JamEntrySummary.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.JamEntry
+{
+    /// <summary>
+    /// Interaction logic for JamEntrySummary.xaml
+    /// </summary>
+    public partial class JamEntrySummary : UserControl
+    {
+        public JamEntrySummary()
+        {
+            InitializeComponent();
+        }
+    }
+}
