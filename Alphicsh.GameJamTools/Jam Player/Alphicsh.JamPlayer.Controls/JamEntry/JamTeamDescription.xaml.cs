fd3fcdd73cbc6a36d08455a123dd1034b1168a86
Alice Jankowska <alphish.creature@gmail.com>
2021-10-31 19:07:08 +0100

    [Feature] Added a control for JamTeamDescription

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/JamEntry/JamTeamDescription.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.JamEntry
+{
+    /// <summary>
+    /// Interaction logic for JamEntryAuthors.xaml
+    /// </summary>
+    public partial class JamTeamDescription : UserControl
+    {
+        public JamTeamDescription()
+        {
+            InitializeComponent();
+        }
+    }
+}
