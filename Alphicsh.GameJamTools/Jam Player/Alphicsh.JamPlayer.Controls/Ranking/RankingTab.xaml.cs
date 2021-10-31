6f8dbe765220da631d7c468f46de0e8b43c2f092
Alice Jankowska <alphish.creature@gmail.com>
2021-10-31 20:09:02 +0100

    [Feature] Created a ranking tab control, putting together all the rankings-related components

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Ranking/RankingTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.Ranking
+{
+    /// <summary>
+    /// Interaction logic for RankingTab.xaml
+    /// </summary>
+    public partial class RankingTab : UserControl
+    {
+        public RankingTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
