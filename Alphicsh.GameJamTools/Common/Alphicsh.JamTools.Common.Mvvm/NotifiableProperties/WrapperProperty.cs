ef3e57f0ce52c93e4c505fa8714acfa660a52d63
Alice Jankowska <alphish.creature@gmail.com>
2021-10-31 13:54:34 +0100

    [Feature] Added a WrapperProperty class for relaying getter and setter methods

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.Mvvm/NotifiableProperties/WrapperProperty.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Alphicsh.JamTools.Common.Mvvm.NotifiableProperties
+{
+    public class WrapperProperty<TViewModel, TValue> : NotifiableProperty
+        where TViewModel : IViewModel
+    {
+        protected new TViewModel ViewModel => (TViewModel)base.ViewModel;
+
+        private Func<TViewModel, TValue> ValueGetter { get; set; }
+        private Action<TViewModel, TValue>? ValueSetter { get; set; }
+
+        public bool IsReadonly => ValueSetter == null;
+        public TValue Value
+        {
+            get => ValueGetter(ViewModel);
+            set
+            {
+                if (ValueSetter == null)
+                    throw new NotSupportedException("Cannot set a value of the readonly wrapper property.");
+
+                ValueSetter.Invoke(ViewModel, value);
+            }
+        }
+
+        public WrapperProperty(
+            TViewModel viewModel, string propertyName, Func<TViewModel, TValue> valueGetter, Action<TViewModel, TValue>? valueSetter
+            ) : base(viewModel, propertyName)
+        {
+            ValueGetter = valueGetter;
+            ValueSetter = valueSetter;
+        }
+    }
+
+    // ---------------
+    // Static creation
+    // ---------------
+
+    public static class WrapperProperty
+    {
+        public static WrapperProperty<TViewModel, TValue> Create<TViewModel, TValue>(
+            TViewModel viewModel, string propertyName, Func<TViewModel, TValue> valueGetter, Action<TViewModel, TValue> valueSetter
+            )
+            where TViewModel : IViewModel
+        {
+            return new WrapperProperty<TViewModel, TValue>(viewModel, propertyName, valueGetter, valueSetter);
+        }
+
+        public static WrapperProperty<TViewModel, TValue> CreateReadonly<TViewModel, TValue>(
+            TViewModel viewModel, string propertyName, Func<TViewModel, TValue> valueGetter
+            )
+            where TViewModel : IViewModel
+        {
+            return new WrapperProperty<TViewModel, TValue>(viewModel, propertyName, valueGetter, valueSetter: null);
+        }
+    }
+}
