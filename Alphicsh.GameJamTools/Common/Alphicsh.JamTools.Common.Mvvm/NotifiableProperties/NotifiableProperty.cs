f23f529760aaec3de72295e945f4a9c72a3b2e25
Alice Jankowska <alphish.creature@gmail.com>
2021-10-31 13:48:24 +0100

    [Refactor] Created INotifiableProperty interface with simple NotifiableProperty implementation

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.Mvvm/NotifiableProperties/NotifiableProperty.cs
@@ -0,0 +1,24 @@
+﻿namespace Alphicsh.JamTools.Common.Mvvm.NotifiableProperties
+{
+    public class NotifiableProperty : INotifiableProperty
+    {
+        protected IViewModel ViewModel { get; }
+        protected string PropertyName { get; }
+
+        public NotifiableProperty(IViewModel viewModel, string propertyName)
+        {
+            ViewModel = viewModel;
+            PropertyName = propertyName;
+        }
+
+        public static NotifiableProperty Create(IViewModel viewModel, string propertyName)
+        {
+            return new NotifiableProperty(viewModel, propertyName);
+        }
+
+        public void RaisePropertyChanged()
+        {
+            ViewModel.RaisePropertyChanged(PropertyName);
+        }
+    }
+}
