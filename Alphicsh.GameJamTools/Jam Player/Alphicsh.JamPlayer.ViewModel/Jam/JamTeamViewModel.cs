66cc459a521ecf91bc1d88f9df9e488ac4818118
Alice Jankowska <alphish.creature@gmail.com>
2021-10-31 19:06:43 +0100

    [Feature] Added generating description items for JamTeamViewModel
    Also, renamed the current Description variable to ShortDescription, with short description being either team or author names, and long description being team AND author names (if team name is available).

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Jam/JamTeamViewModel.cs
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Jam/JamTeamViewModel.cs
@@ -3,6 +3,7 @@
 using Alphicsh.JamTools.Common.Mvvm;
 
 using Alphicsh.JamPlayer.Model.Jam;
+using System.Collections.Generic;
 
 namespace Alphicsh.JamPlayer.ViewModel.Jam
 {
@@ -12,11 +13,39 @@ namespace Alphicsh.JamPlayer.ViewModel.Jam
             : base(model)
         {
             Authors = CollectionViewModel.CreateImmutable(model.Authors, JamAuthorViewModel.CollectionStub);
-            Description = Model.Name ?? string.Join(", ", Authors.Select(author => author.Name));
+            ShortDescription = Model.Name ?? string.Join(", ", Authors.Select(author => author.Name));
+
+            DescriptionItems = BuildDescriptionItems();
         }
 
         public string? Name => Model.Name;
         public CollectionViewModel<JamAuthor, JamAuthorViewModel> Authors { get; }
-        public string Description { get; }
+        public string ShortDescription { get; }
+
+        // -----------------
+        // Description items
+        // -----------------
+
+        public IReadOnlyCollection<object> DescriptionItems { get; }
+
+        private IReadOnlyCollection<object> BuildDescriptionItems()
+        {
+            var displayItems = new List<object>();
+
+            foreach (var author in Authors)
+            {
+                displayItems.Add(author);
+                if (author != Authors.Last())
+                    displayItems.Add(", ");
+            }
+            if (Name != null)
+            {
+                displayItems.Insert(0, this);
+                displayItems.Insert(1, " (");
+                displayItems.Add(")");
+            }
+
+            return displayItems;
+        }
     }
 }
