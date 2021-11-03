1959d773adc68830790cd43312733f8ff7437428
Alice Jankowska <alphish.creature@gmail.com>
2021-11-03 07:42:13 +0100

    [Feature] Created classes for JSON content serialization/deserialization
    On that note, I appreciate how with the introduction of System.Text.Json I don't need a package dependency of Newtonsoft.Json anymore.

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/IContentSerializer.cs
@@ -0,0 +1,8 @@
+﻿namespace Alphicsh.JamTools.Common.IO
+{
+    public interface IContentSerializer<TEntity>
+    {
+        string Serialize(TEntity entity);
+        TEntity? Deserialize(string content);
+    }
+}
---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/JsonContentSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Alphicsh.JamTools.Common.IO
+{
+    public class JsonContentSerializer<TEntity> : IContentSerializer<TEntity>
+    {
+        protected JsonSerializerOptions SerializerOptions { get; } = new JsonSerializerOptions()
+        {
+            // allowing flexible JSONs
+            AllowTrailingCommas = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+
+            // ignoring properties
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            IncludeFields = false,
+            IgnoreReadOnlyProperties = true,
+
+            // property names
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+
+            // indentation
+            WriteIndented = true,
+        };
+
+        public TEntity? Deserialize(string content)
+        {
+            return JsonSerializer.Deserialize<TEntity>(content, SerializerOptions);
+        }
+
+        public string Serialize(TEntity entity)
+        {
+            return JsonSerializer.Serialize(entity, SerializerOptions);
+        }
+    }
+}
