e0cabff0b34a1c364c9ee51a5ffe475765c88827
Alice Jankowska <alphish.creature@gmail.com>
2021-11-03 07:50:08 +0100

    [Feature] Created a files reader and explorer for Jam entry

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Jam/Files/JamEntryFilesReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Alphicsh.JamTools.Common.IO.Jam.Files
+{
+    public class JamEntryFilesReader
+    {
+        private JsonContentSerializer<JamEntryInfo> JamEntryInfoSerializer { get; }
+
+        public JamEntryFilesReader()
+        {
+            JamEntryInfoSerializer = new JsonContentSerializer<JamEntryInfo>();
+        }
+
+        public JamEntryInfo? TryLoadJamEntryInfo(string entryId, FilePath filePath)
+        {
+            if (filePath.IsRelative())
+                throw new ArgumentException("The jam entry info can only be read from the absolute file path.", nameof(filePath));
+
+            var content = File.ReadAllText(filePath.Value);
+            var jamEntryInfo = JamEntryInfoSerializer.Deserialize(content);
+            if (jamEntryInfo == null)
+                return null;
+
+            jamEntryInfo.Id = entryId;
+            jamEntryInfo.EntryInfoPath = filePath;
+            jamEntryInfo.EntryDirectoryPath = filePath.GetParentDirectoryPath()!.Value;
+
+            return jamEntryInfo;
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Jam/Files/JamEntryInfoExplorer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+using Alphicsh.JamTools.Common.IO.Search;
+
+namespace Alphicsh.JamTools.Common.IO.Jam.Files
+{
+    public class JamEntryInfoExplorer
+    {
+        private JamEntryFilesReader EntryFilesReader { get; }
+
+        public JamEntryInfoExplorer()
+        {
+            EntryFilesReader = new JamEntryFilesReader();
+        }
+
+        public JamEntryInfo FindJamEntryInfo(string id, FilePath entryDirectoryPath)
+        {
+            return GetEntryInfoFromFile(id, entryDirectoryPath)
+                ?? StubEntryInfoFromDirectory(id, entryDirectoryPath);
+        }
+
+        // -----------------
+        // Loading from file
+        // -----------------
+
+        private JamEntryInfo? GetEntryInfoFromFile(string id, FilePath entryDirectoryPath)
+        {
+            var jamEntryPaths = FilesystemSearch.ForFilesIn(entryDirectoryPath)
+                .IncludingTopDirectoryOnly()
+                .WithExtensions(".jamentry")
+                .FindAll()
+                .FoundPaths;
+
+            if (!jamEntryPaths.Any())
+                return null;
+
+            var entryInfoPath = jamEntryPaths.First();
+            return EntryFilesReader.TryLoadJamEntryInfo(id, entryInfoPath);
+        }
+
+        // -----------------------
+        // Stubbing from directory
+        // -----------------------
+
+        private JamEntryInfo StubEntryInfoFromDirectory(string id, FilePath entryDirectoryPath)
+        {
+            var entryDirectoryName = entryDirectoryPath.GetLastSegmentName();
+            var byIndex = entryDirectoryName.LastIndexOf(" by ");
+
+            string title;
+            string[] authors;
+            if (byIndex == -1)
+            {
+                title = entryDirectoryName;
+                authors = new[] { "??? " };
+            }
+            else
+            {
+                title = entryDirectoryName.Remove(byIndex);
+                var authorsString = entryDirectoryName.Substring(byIndex + " by ".Length);
+                authors = authorsString.Split(",", StringSplitOptions.TrimEntries);
+            }
+
+            return new JamEntryInfo()
+            {
+                Id = id,
+                EntryDirectoryPath = entryDirectoryPath,
+                Title = title,
+                Team = new JamTeamInfo
+                {
+                    Name = null,
+                    Authors = authors.Select(name => new JamAuthorInfo { Name = name }).ToList()
+                }
+            };
+        }
+    }
+}
