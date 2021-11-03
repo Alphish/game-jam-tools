93579260cfb86029318025052f74ad20baf26fdb
Alice Jankowska <alphish.creature@gmail.com>
2021-11-03 22:00:03 +0100

    [Feature] Added JSON converters for FilePath struct
    Also, included them in standard options of JsonContentSerializer.

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/FilePathJsonConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Alphicsh.JamTools.Common.IO
+{
+    // ------------
+    // Non-nullable
+    // ------------
+
+    public class FilePathJsonConverter : JsonConverter<FilePath>
+    {
+        public override FilePath Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return FilePath.From(reader.GetString()!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, FilePath value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.Value);
+        }
+    }
+
+    // --------
+    // Nullable
+    // --------
+
+    public class NullableFilePathJsonConverter : JsonConverter<FilePath?>
+    {
+        public override FilePath? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return FilePath.FromNullable(reader.GetString());
+        }
+
+        public override void Write(Utf8JsonWriter writer, FilePath? value, JsonSerializerOptions options)
+        {
+            if (value == null)
+                writer.WriteNullValue();
+
+            writer.WriteStringValue(value!.Value.Value); // that's a lot of values
+        }
+    }
+}
--- Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/JsonContentSerializer.cs
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/JsonContentSerializer.cs
@@ -5,24 +5,42 @@ namespace Alphicsh.JamTools.Common.IO
 {
     public class JsonContentSerializer<TEntity> : IContentSerializer<TEntity>
     {
-        protected JsonSerializerOptions SerializerOptions { get; } = new JsonSerializerOptions()
+        // -----
+        // Setup
+        // -----
+
+        protected JsonSerializerOptions SerializerOptions { get; }
+
+        public JsonContentSerializer()
         {
-            // allowing flexible JSONs
-            AllowTrailingCommas = true,
-            ReadCommentHandling = JsonCommentHandling.Skip,
+            SerializerOptions = new JsonSerializerOptions()
+            {
+                // allowing flexible JSONs
+                AllowTrailingCommas = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
 
-            // ignoring properties
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            IncludeFields = false,
-            IgnoreReadOnlyProperties = true,
+                // ignoring properties
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                IncludeFields = false,
+                IgnoreReadOnlyProperties = true,
 
-            // property names
-            PropertyNameCaseInsensitive = true,
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                // property names
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
 
-            // indentation
-            WriteIndented = true,
-        };
+                // indentation
+                WriteIndented = true,
+
+            };
+
+            // converters
+            SerializerOptions.Converters.Add(new FilePathJsonConverter());
+            SerializerOptions.Converters.Add(new NullableFilePathJsonConverter());
+        }
+
+        // -------
+        // Methods
+        // -------
 
         public TEntity? Deserialize(string content)
         {
