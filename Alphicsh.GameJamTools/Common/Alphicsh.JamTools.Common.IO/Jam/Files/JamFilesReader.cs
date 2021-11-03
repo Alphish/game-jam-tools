dfd80900bdf8208a095931b68112cf794ace75e0
Alice Jankowska <alphish.creature@gmail.com>
2021-11-03 07:54:12 +0100

    [Feature] Created a files reader and explorer for the Jam

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Jam/Files/JamFilesReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Alphicsh.JamTools.Common.IO.Jam.Files
+{
+    public class JamFilesReader
+    {
+        private JsonContentSerializer<JamInfo> JamInfoSerializer { get; }
+
+        public JamFilesReader()
+        {
+            JamInfoSerializer = new JsonContentSerializer<JamInfo>();
+        }
+
+        public JamInfo? TryLoadJamInfo(FilePath filePath)
+        {
+            if (filePath.IsRelative())
+                throw new ArgumentException("The jam info can only be read from the absolute file path.", nameof(filePath));
+
+            var content = File.ReadAllText(filePath.Value);
+            var jamInfo = JamInfoSerializer.Deserialize(content);
+            if (jamInfo == null)
+                return null;
+
+            jamInfo.JamInfoPath = filePath;
+            jamInfo.JamDirectoryPath = filePath.GetParentDirectoryPath()!.Value;
+
+            return jamInfo;
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Jam/Files/JamInfoExplorer.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Alphicsh.JamTools.Common.IO.Search;
+
+namespace Alphicsh.JamTools.Common.IO.Jam.Files
+{
+    public class JamInfoExplorer
+    {
+        private JamFilesReader JamFilesReader { get; }
+        private JamEntryInfoExplorer EntryInfoExplorer { get; }
+
+        public JamInfoExplorer()
+        {
+            JamFilesReader = new JamFilesReader();
+            EntryInfoExplorer = new JamEntryInfoExplorer();
+        }
+
+        public JamInfo FindJamInfo(FilePath jamDirectoryPath)
+        {
+            var jamInfo = StubJamInfoFromFile(jamDirectoryPath)
+                ?? StubJamInfoFromDirectory(jamDirectoryPath);
+
+            var entriesPath = jamDirectoryPath.Append(jamInfo.EntriesSubpath);
+            jamInfo.Entries = GetEntriesFromStubs(entriesPath, jamInfo.EntriesStubs).ToList();
+
+            return jamInfo;
+        }
+
+        // ------------------
+        // Stubbing from file
+        // ------------------
+
+        private JamInfo? StubJamInfoFromFile(FilePath jamDirectoryPath)
+        {
+            var jamInfoPaths = FilesystemSearch.ForFilesIn(jamDirectoryPath)
+                .IncludingTopDirectoryOnly()
+                .WithExtensions(".jaminfo")
+                .FindAll()
+                .FoundPaths;
+
+            if (!jamInfoPaths.Any())
+                return null;
+
+            var jamInfoPath = jamInfoPaths.First();
+
+            return JamFilesReader.TryLoadJamInfo(jamInfoPath);
+        }
+
+        // -----------------------
+        // Stubbing from directory
+        // -----------------------
+
+        private JamInfo StubJamInfoFromDirectory(FilePath jamDirectoryPath)
+        {
+            var entriesPath = FindEntriesPath(jamDirectoryPath);
+            if (entriesPath == null)
+                throw new DirectoryNotFoundException("Could not find the entries subdirectory in the jam directory.");
+
+            var entriesStubs = FindEntriesStubs(entriesPath.Value).ToList();
+
+            return new JamInfo()
+            {
+                JamDirectoryPath = jamDirectoryPath,
+                EntriesSubpath = entriesPath.Value.AsRelativeTo(jamDirectoryPath),
+                EntriesStubs = entriesStubs,
+            };
+        }
+
+        private FilePath? FindEntriesPath(FilePath jamDirectoryPath)
+        {
+            var entriesPaths = FilesystemSearch.ForDirectoriesIn(jamDirectoryPath)
+                .IncludingTopDirectoryOnly()
+                .FindMatches("Entries")
+                .ElseFindMatches("*Entries*")
+                .FoundPaths;
+
+            return entriesPaths.Any() ? entriesPaths.First() : null;
+        }
+
+        private IEnumerable<JamEntryStub> FindEntriesStubs(FilePath entriesPath)
+        {
+            var jamEntries = FilesystemSearch.ForDirectoriesIn(entriesPath)
+                .IncludingTopDirectoryOnly()
+                .FindAll()
+                .FoundPaths;
+
+            foreach (var jamEntryPath in jamEntries)
+            {
+                var subpath = jamEntryPath.AsRelativeTo(entriesPath);
+                yield return new JamEntryStub
+                {
+                    Id = subpath.Value,
+                    EntrySubpath = subpath.Value,
+                };
+            }
+        }
+
+        // ---------------
+        // Loading entries
+        // ---------------
+
+        private IEnumerable<JamEntryInfo> GetEntriesFromStubs(FilePath entriesPath, IReadOnlyCollection<JamEntryStub> stubs)
+        {
+            foreach (var stub in stubs)
+            {
+                var id = stub.Id;
+                var entryDirectoryPath = entriesPath.Append(stub.EntrySubpath);
+                yield return EntryInfoExplorer.FindJamEntryInfo(id, entryDirectoryPath);
+            }
+        }
+    }
+}
