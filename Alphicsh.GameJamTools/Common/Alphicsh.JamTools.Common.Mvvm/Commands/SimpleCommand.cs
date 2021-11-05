7de6b21e061cac55c576311ba859967c7e49f846
Alice Jankowska <alphish.creature@gmail.com>
2021-11-05 09:14:05 +0100

    [Feature] Created a SimpleCommand class for always available parameterless commands

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.Mvvm/Commands/SimpleCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Alphicsh.JamTools.Common.Mvvm.Commands
+{
+    public class SimpleCommand : ICommand
+    {
+        private Action ExecutionAction { get; }
+
+        // --------
+        // Creation
+        // --------
+
+        public SimpleCommand(Action executionAction)
+        {
+            ExecutionAction = executionAction;
+        }
+
+        public static SimpleCommand From(Action executionAction)
+        {
+            return new SimpleCommand(executionAction);
+        }
+
+        // -----------------------
+        // ICommand implementation
+        // -----------------------
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            ExecutionAction();
+        }
+    }
+}
