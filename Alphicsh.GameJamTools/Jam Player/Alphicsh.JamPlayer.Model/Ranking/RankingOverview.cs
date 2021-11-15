680891bc7e1668ba0388c963bf1b4673c8edce3a
Alice Jankowska <alphish.creature@gmail.com>
2021-11-16 00:17:09 +0100

    [Feature] Added the pending entries list to ranking overview, along with a function to add the next entry
    Also, removed the SelectedEntry variable because the view model has its own.

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Ranking/RankingOverview.cs
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Ranking/RankingOverview.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace Alphicsh.JamPlayer.Model.Ranking
 {
     public sealed class RankingOverview
     {
+        public IList<RankingEntry> PendingEntries { get; set; }
+        
         public IList<RankingEntry> RankedEntries { get; set; }
         public IList<RankingEntry> UnrankedEntries { get; set; }
 
-        public RankingEntry SelectedEntry { get; set; }
-
         public RankingOverview()
         {
+            PendingEntries = new List<RankingEntry>();
             RankedEntries = new List<RankingEntry>();
             UnrankedEntries = new List<RankingEntry>();
+        }
+
+        public RankingEntry? GetNextEntry()
+        {
+            if (!PendingEntries.Any())
+                return null;
+
+            var index = RandomNumberGenerator.GetInt32(fromInclusive: 0, toExclusive: PendingEntries.Count);
+            var entry = PendingEntries[index];
+
+            PendingEntries.Remove(entry);
+            UnrankedEntries.Add(entry);
+            return entry;
         }
     }
 }
