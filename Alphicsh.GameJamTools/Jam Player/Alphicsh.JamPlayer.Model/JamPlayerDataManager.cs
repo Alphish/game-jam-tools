4b0d75929efedda031837569eb276a130ff2df6a
Alice Jankowska <alphish.creature@gmail.com>
2021-11-16 23:16:08 +0100

    [Feature] Created the JamPlayerDataManager class

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/JamPlayerDataManager.cs
@@ -0,0 +1,10 @@
+﻿using Alphicsh.JamTools.Common.IO;
+
+namespace Alphicsh.JamPlayer.Model
+{
+    public class JamPlayerDataManager
+    {
+        public AppModel AppModel { get; init; } = default!;
+        public FilePath DirectoryPath => AppModel.Jam.DirectoryPath.Append(".jamplayer");
+    }
+}
