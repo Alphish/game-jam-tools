0a80cc0527e258f475dc6c26c8d3d9124ff4089a
Alice Jankowska <alphish.creature@gmail.com>
2021-11-16 23:05:54 +0100

    [Refactor] Replaced NumericScaleOptions with NumericScaleCriterion

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Ratings/NumericScale/NumericScaleOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Alphicsh.JamPlayer.Model.Ratings.NumericScale
-{
-    public class NumericScaleOptions : IRatingOptions<double?>
-    {
-        public double ValueStep { get; set; }
-        public double MaxValue { get; set; }
-        public INumericScaleSkin Skin { get; set; } = default!;
-    }
-}
