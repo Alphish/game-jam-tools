9c29d4193fa3339f5b7e4b435853d427ebe66d01
Alice Jankowska <alphish.creature@gmail.com>
2021-11-16 23:33:43 +0100

    [Fix] Added the missing SaveRanking method on the JamPlayerDataManager

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/JamPlayerDataManager.cs
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/JamPlayerDataManager.cs
@@ -1,10 +1,26 @@
 ﻿using Alphicsh.JamTools.Common.IO;
 
+using Alphicsh.JamPlayer.IO.Ranking;
+using Alphicsh.JamPlayer.Model.Ranking;
+
 namespace Alphicsh.JamPlayer.Model
 {
     public class JamPlayerDataManager
     {
         public AppModel AppModel { get; init; } = default!;
         public FilePath DirectoryPath => AppModel.Jam.DirectoryPath.Append(".jamplayer");
+
+        // --------
+        // Rankings
+        // --------
+
+        public FilePath RankingPath => DirectoryPath.Append("ranking.jamranking");
+        private RankingInfoMapper RankingInfoMapper { get; } = new RankingInfoMapper();
+
+        public void SaveRanking()
+        {
+            var rankingInfo = RankingInfoMapper.MapRankingToInfo(AppModel.Ranking);
+            rankingInfo.SaveTo(RankingPath);
+        }
     }
 }
