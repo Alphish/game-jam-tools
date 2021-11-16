b87c8f1042ab0f1ea470262673c33c7810762ce9
Alice Jankowska <alphish.creature@gmail.com>
2021-11-16 23:31:09 +0100

    [Feature] Created the app-model bound ranking toolbox control

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Ranking/RankingToolbox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.Ranking
+{
+    /// <summary>
+    /// Interaction logic for RankingToolbox.xaml
+    /// </summary>
+    public partial class RankingToolbox : UserControl
+    {
+        public RankingToolbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
