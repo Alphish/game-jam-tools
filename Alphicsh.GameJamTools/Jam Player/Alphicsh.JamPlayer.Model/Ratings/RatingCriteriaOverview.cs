c9344c8bc5dc0a84f70eefa8130d62e0a2935f7b
Alice Jankowska <alphish.creature@gmail.com>
2021-11-16 23:07:08 +0100

    [Feature] Defined a structure for rating criteria overview

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Ratings/RatingCriteriaOverview.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphicsh.JamPlayer.Model.Ratings
+{
+    public class RatingCriteriaOverview
+    {
+        public IReadOnlyCollection<IRatingSkin> Skins { get; set; } = default!;
+        public IReadOnlyCollection<IRatingCriterion> Criteria { get; set; } = default!;
+    }
+}
