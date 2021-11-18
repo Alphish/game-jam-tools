e4bb41aebddfb9ff1a4d932ab4818c4877c19dd0
Alice Jankowska <alphish.creature@gmail.com>
2021-11-18 18:53:45 +0100

    [Feature] Created function parameter and function parameter list classes for storing and matching function parameters

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/FunctionParameter.cs
@@ -0,0 +1,17 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public class FunctionParameter
+    {
+        public CodeName Name { get; }
+        public IPrototype Prototype { get; }
+
+        public FunctionParameter(CodeName name, IPrototype prototype)
+        {
+            Name = name;
+            Prototype = prototype;
+        }
+
+        public static FunctionParameter Create(string name, IPrototype prototype)
+            => new FunctionParameter(CodeName.From(name), prototype);
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/FunctionParameterList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public class FunctionParameterList : IReadOnlyCollection<FunctionParameter>
+    {
+        private IReadOnlyCollection<FunctionParameter> Items { get; }
+
+        public FunctionParameterList(IEnumerable<FunctionParameter> parameters)
+        {
+            Items = parameters.ToList();
+            EnsureParametersAreValid(Items);
+        }
+
+        private void EnsureParametersAreValid(IReadOnlyCollection<FunctionParameter> parameters)
+        {
+            // checking for duplicate parameter names
+            var parameterNames = parameters
+                .GroupBy(parameter => parameter.Name)
+                .Where(group => group.Skip(1).Any())
+                .Select(group => group.Key)
+                .ToList();
+
+            if (parameterNames.Any())
+            {
+                throw new ArgumentException(
+                    "Duplicate function parameter names: " + string.Join(", ", parameterNames),
+                    nameof(parameters)
+                );
+            }
+        }
+
+        // ------
+        // Checks
+        // ------
+
+        public bool MatchesArgumentTypes(IEnumerable<IPrototype> declaredTypes)
+        {
+            var remainingParameters = Items.ToList();
+            foreach (var type in declaredTypes)
+            {
+                if (!remainingParameters.Any())
+                    return false; // too many arguments given
+                
+                var matchingParameter = remainingParameters.First();
+                remainingParameters.RemoveAt(0);
+                if (!type.IsSubtypeOf(matchingParameter.Prototype))
+                    return false; // invalid argument type
+            }
+
+            if (remainingParameters.Any())
+                return false; // some expected arguments are missing
+
+            return true;
+        }
+
+        // -------------------
+        // IReadOnlyCollection
+        // -------------------
+
+        public int Count => Items.Count;
+
+        public IEnumerator<FunctionParameter> GetEnumerator()
+            => Items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
