42224d42eca04d90706971f4eb650d120a64af5f
Alice Jankowska <alphish.creature@gmail.com>
2021-11-18 18:52:23 +0100

    [Feature] Created the IPrototype interface to make a proto-type system in the export runtime

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/IPrototype.cs
@@ -0,0 +1,9 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime
+{
+    public interface IPrototype
+    {
+        CodeName Name { get; }
+        
+        bool IsSubtypeOf(IPrototype matchedType);
+    }
+}
