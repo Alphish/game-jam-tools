235f4bb024942c10d2995d601f0a7a0894ac219c
Alice Jankowska <alphish.creature@gmail.com>
2021-11-19 00:16:50 +0100

    [Feature] Created a base function class, with basic properties and input and output type checking

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/BaseFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public abstract class BaseFunction : IFunction
+    {
+        public FunctionParameterList Parameters { get; }
+        public IPrototype ReturnType { get; }
+
+        protected BaseFunction(IEnumerable<FunctionParameter> parameters, IPrototype returnType)
+        {
+            Parameters = new FunctionParameterList(parameters);
+            ReturnType = returnType;
+        }
+
+        public IInstance Call(IEnumerable<IInstance> arguments)
+        {
+            var argumentsList = arguments as IReadOnlyCollection<IInstance> ?? arguments.ToList();
+            
+            var argumentTypes = argumentsList.Select(argument => argument.Prototype);
+            if (!Parameters.MatchesArgumentTypes(argumentTypes))
+                throw new ArgumentException("The given argument types don't match the function parameters.", nameof(arguments));
+
+            var result = DoCall(argumentsList);
+            if (!result.Prototype.IsSubtypeOf(ReturnType))
+                throw new InvalidCastException("The result returned by the function doesn't match declared return type.");
+
+            return result;
+        }
+
+        protected abstract IInstance DoCall(IEnumerable<IInstance> arguments);
+    }
+}
