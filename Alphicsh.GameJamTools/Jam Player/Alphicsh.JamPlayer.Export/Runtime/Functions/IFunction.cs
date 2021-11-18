04faf092d47d0e80cec67ec663ad8bfe2e1adc87
Alice Jankowska <alphish.creature@gmail.com>
2021-11-19 00:16:18 +0100

    [Feature] Created the IFunction interface to represent the runtime calls

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/IFunction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public interface IFunction
+    {
+        FunctionParameterList Parameters { get; }
+        IPrototype ReturnType { get; }
+
+        IInstance Call(IEnumerable<IInstance> arguments);
+    }
+}
