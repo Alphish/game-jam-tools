0ca6b3aa81bc6e9502f6902ac491e3206562ca4b
Alice Jankowska <alphish.creature@gmail.com>
2021-11-18 18:55:42 +0100

    [Tests] Created a dummy IPrototype impementation for testing export runtime functions

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export.Tests/Runtime/Functions/FunctionTestPrototype.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public class FunctionTestPrototype : IPrototype
+    {
+        // --------
+        // Creation
+        // --------
+        
+        public CodeName Name { get; }
+        private HashSet<CodeName> SupertypeNames { get; }
+
+        private FunctionTestPrototype(string prototypeName, IEnumerable<string> supertypeNames)
+        {
+            Name = CodeName.From(prototypeName);
+            SupertypeNames = supertypeNames.Select(CodeName.From).ToHashSet();
+        }
+
+        public static FunctionTestPrototype Lorem { get; }
+            = new FunctionTestPrototype("lorem", Enumerable.Empty<string>());
+        public static FunctionTestPrototype Ipsum { get; }
+            = new FunctionTestPrototype("ipsum", Enumerable.Empty<string>());
+        public static FunctionTestPrototype SubIpsum { get; }
+            = new FunctionTestPrototype("sub_ipsum", supertypeNames: new string[] { "ipsum" });
+        
+        public bool IsSubtypeOf(IPrototype matchedType)
+        {
+            return matchedType == this || SupertypeNames.Contains(matchedType.Name);
+        }
+    }
+}
