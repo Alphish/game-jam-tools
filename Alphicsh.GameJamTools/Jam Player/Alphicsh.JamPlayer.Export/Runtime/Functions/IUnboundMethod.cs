084fd498ca31f6097925b2628e10ea29fb8e6358
Alice Jankowska <alphish.creature@gmail.com>
2021-11-19 22:44:44 +0100

    [Feature] Created the UnboundMethod class for functions that have yet to be associated with an instance

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/IUnboundMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public interface IUnboundMethod
+    {
+        IPrototype ReturnType { get; }
+        IFunction Bind(IInstance instance);
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/UnboundMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public class UnboundMethod : IUnboundMethod
+    {
+        public IPrototype ReturnType { get; }
+
+        private IReadOnlyCollection<FunctionParameter> Parameters { get; }
+        private InstanceMethodCallback Callback { get; }
+        
+        public UnboundMethod(
+            IEnumerable<FunctionParameter> parameters,
+            IPrototype returnType,
+            InstanceMethodCallback callback
+        )
+        {
+            Parameters = parameters.ToList();
+            ReturnType = returnType;
+            Callback = callback;
+        }
+        
+        public IFunction Bind(IInstance instance)
+        {
+            return new InstanceMethod(Parameters, ReturnType, instance, Callback);
+        }
+    }
+}
