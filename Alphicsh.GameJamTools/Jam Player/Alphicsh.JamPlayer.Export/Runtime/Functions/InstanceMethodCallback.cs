9b77adaf283ebcfab834139e3d5116dd4a8e6952
Alice Jankowska <alphish.creature@gmail.com>
2021-11-19 22:45:56 +0100

    [Fix] Added the missing InstanceMethodCallback delegate

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/InstanceMethodCallback.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public delegate IInstance InstanceMethodCallback(IInstance instance, IEnumerable<IInstance> arguments);
+}
