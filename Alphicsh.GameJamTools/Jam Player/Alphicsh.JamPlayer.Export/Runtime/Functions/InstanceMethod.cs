19db757b4e3d61dbf97e5733f77ddae8133fba48
Alice Jankowska <alphish.creature@gmail.com>
2021-11-20 10:23:51 +0100

    [Feature] Created the InstanceMethod implementation of IFunction, which takes into account both the instance bound and the arguments given

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/InstanceMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public class InstanceMethod : BaseFunction
+    {
+        private IInstance Instance { get; }
+        private InstanceMethodCallback Callback { get; }
+        
+        public InstanceMethod(
+            IEnumerable<FunctionParameter> parameters,
+            IPrototype returnType,
+            IInstance instance,
+            InstanceMethodCallback callback
+        ) : base(parameters, returnType)
+        {
+            Instance = instance;
+            Callback = callback;
+        }
+
+        protected override IInstance DoCall(IEnumerable<IInstance> arguments)
+        {
+            return Callback.Invoke(Instance, arguments);
+        }
+    }
+}
