db45c1865d10e8be11fa3e5049861c8b7ce0e407
Alice Jankowska <alphish.creature@gmail.com>
2021-11-22 00:49:03 +0100

    [Tests] Created TestDummyInstance and TestDummyPrototype for the sake of BasePrototype/Instance testing

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export.Tests/Runtime/Common/TestDummyInstance.cs
@@ -0,0 +1,12 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Common
+{
+    public class TestDummyInstance : BaseInstance<TestDummyInstance>
+    {
+        public int Value { get; }
+        
+        public TestDummyInstance(int value) : base(TestDummyPrototype.Prototype)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export.Tests/Runtime/Common/TestDummyPrototype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Common
+{
+    public class TestDummyPrototype : BasePrototype<TestDummyInstance>
+    {
+        // --------
+        // Creation
+        // --------
+        
+        public static TestDummyPrototype Prototype { get; } = new TestDummyPrototype();
+
+        private TestDummyPrototype()
+        {
+        }
+
+        public TestDummyInstance CreateInstance(int value = 0)
+            => new TestDummyInstance(value);
+
+        // ---------
+        // Overrides
+        // ---------
+
+        public override TypeName Name { get; } = TypeName.CreateSimple("TestDummy");
+    }
+}
