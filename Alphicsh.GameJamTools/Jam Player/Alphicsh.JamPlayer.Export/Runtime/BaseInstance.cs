33ce0c2391fd4eee28f31d02a4f7e604bd9b007d
Alice Jankowska <alphish.creature@gmail.com>
2021-11-22 00:48:32 +0100

    [Feature] Created a BasePrototype and BaseInstance classes serving as bases for more specific types

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/BaseInstance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Export.Runtime
+{
+    public class BaseInstance<TInstance> : IInstance<TInstance>
+        where TInstance : IInstance<TInstance>
+    {
+        public IPrototype<TInstance> Prototype { get; }
+
+        IPrototype IInstance.Prototype => this.Prototype;
+
+        public BaseInstance(IPrototype<TInstance> prototype)
+        {
+            Prototype = prototype;
+        }
+
+        public IInstance GetMember(CodeName codeName)
+            => Prototype.GetMember(this, codeName);
+        public IInstance GetItem(int index)
+            => Prototype.GetItem(this, index);
+        public IInstance Call(IEnumerable<IInstance> arguments)
+            => Prototype.Call(this, arguments);
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/BasePrototype.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime
+{
+    public abstract partial class BasePrototype<TInstance> : IPrototype<TInstance>
+        where TInstance : IInstance
+    {
+        public abstract TypeName Name { get; }
+
+        public override string ToString()
+            => $"Prototype of {Name}";
+
+        public virtual bool IsSubtypeOf(IPrototype matchedType)
+        {
+            return matchedType == this;
+        }
+        
+        // -------
+        // Members
+        // -------
+        
+        public virtual IPrototype GetMemberType(CodeName memberName)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public virtual IInstance GetMember(TInstance instance, CodeName memberName)
+        {
+            throw new NotImplementedException();
+        }
+
+        IInstance IPrototype.GetMember(IInstance instance, CodeName memberName)
+            => this.GetMember((TInstance) instance, memberName);
+
+        // -----
+        // Items
+        // -----
+
+        public virtual IPrototype GetItemType()
+            => throw new NotSupportedException($"The instances of prototype '{Name}' don't have items.");
+        public virtual IInstance GetItem(TInstance instance, int index)
+            => throw new NotSupportedException($"The instances of prototype '{Name}' don't have items.");
+
+        IInstance IPrototype.GetItem(IInstance instance, int index)
+            => this.GetItem((TInstance) instance, index);
+        
+        // --------------
+        // Function calls
+        // --------------
+
+        public virtual IPrototype? GetCallReturnType(IEnumerable<IPrototype> argumentTypes)
+            => throw new NotSupportedException($"The instances of prototype '{Name}' cannot be called.");
+        public virtual IInstance Call(TInstance instance, IEnumerable<IInstance> arguments)
+            => throw new NotSupportedException($"The instances of prototype '{Name}' cannot be called.");
+
+        IInstance IPrototype.Call(IInstance instance, IEnumerable<IInstance> arguments)
+            => this.Call((TInstance) instance, arguments);
+    }
+}
