765daa282421c40d494a8048187dcc65cc2f3519
Alice Jankowska <alphish.creature@gmail.com>
2021-11-22 23:59:13 +0100

    [Feature] Created StringPrototype and StringInstance

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Strings/StringInstance.cs
@@ -0,0 +1,13 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Strings
+{
+    public class StringInstance : BaseInstance<StringInstance>
+    {
+        public string InnerString { get; }
+        
+        public StringInstance(string innerString)
+            : base(StringPrototype.Prototype)
+        {
+            InnerString = innerString;
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Strings/StringPrototype.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Strings
+{
+    public class StringPrototype : BasePrototype<StringInstance>
+    {
+        // --------
+        // Creation
+        // --------
+
+        static StringPrototype()
+        {
+            Prototype = new StringPrototype();
+            Prototype.Init();
+        }
+        
+        public static StringPrototype Prototype { get; }
+        private StringPrototype() { }
+        private void Init()
+        {
+            DefineMethod("toUpper").WithNoParameters()
+                .Returning(StringPrototype.Prototype)
+                .Executing(StringPrototype.ToUpper)
+                .Complete();
+                
+            DefineMethod("toLower").WithNoParameters()
+                .Returning(StringPrototype.Prototype)
+                .Executing(StringPrototype.ToLower)
+                .Complete();
+        }
+
+        // ---------
+        // Overrides
+        // ---------
+
+        public override TypeName Name { get; } = TypeName.CreateSimple("String");
+        
+        // -------
+        // Methods
+        // -------
+
+        public static IInstance ToUpper(IInstance instance, IEnumerable<IInstance> arguments)
+        {
+            var stringInstance = (StringInstance)instance;
+            return new StringInstance(stringInstance.InnerString.ToUpper());
+        }
+        
+        public static IInstance ToLower(IInstance instance, IEnumerable<IInstance> arguments)
+        {
+            var stringInstance = (StringInstance)instance;
+            return new StringInstance(stringInstance.InnerString.ToLower());
+        }
+    }
+}
