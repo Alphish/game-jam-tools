6aeabfc0e57fa7aa09cc79b81ed3bd4f3815f534
Alice Jankowska <alphish.creature@gmail.com>
2021-11-22 21:07:28 +0100

    [Feature] Created an extension method on IFunction to build a FunctionInstance wrapping it

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Functions/FunctionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Functions
+{
+    public static class FunctionExtensions
+    {
+        public static IInstance ToFunctionInstance(this IFunction function)
+        {
+            return new FunctionInstance(function);
+        }
+    }
+}
