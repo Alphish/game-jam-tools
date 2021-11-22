53bdf2b038c7fd5d3f308e3e6710efbadd70efc6
Alice Jankowska <alphish.creature@gmail.com>
2021-11-22 23:37:08 +0100

    [Fix] Added the missing partial class with the BasePrototype method building

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/BasePrototype_MethodBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime
+{
+    public partial class BasePrototype<TInstance>
+    {
+        protected MethodBuilder DefineMethod(string methodName)
+        {
+            return new MethodBuilder(this, CodeName.From(methodName));
+        }
+        
+        protected class MethodBuilder
+        {
+            private BasePrototype<TInstance> Prototype { get; }
+            private CodeName MethodName { get; }
+
+            private ICollection<FunctionParameter> Parameters { get; } = new List<FunctionParameter>();
+            private IPrototype ReturnType { get; set; } = default!;
+            private InstanceMethodCallback Callback { get; set; } = default!;
+        
+            public MethodBuilder(BasePrototype<TInstance> prototype, CodeName methodName)
+            {
+                Prototype = prototype;
+                MethodName = methodName;
+            }
+
+            public MethodBuilder WithParameter(string name, IPrototype prototype)
+            {
+                Parameters.Add(FunctionParameter.Create(name, prototype));
+                return this;
+            }
+
+            public MethodBuilder WithNoParameters()
+            {
+                return this;
+            }
+
+            public MethodBuilder Returning(IPrototype returnType)
+            {
+                ReturnType = returnType;
+                return this;
+            }
+
+            public MethodBuilder Executing(InstanceMethodCallback callback)
+            {
+                Callback = callback;
+                return this;
+            }
+
+            public void Complete()
+            {
+                var method = new UnboundMethod(Parameters, ReturnType, Callback);
+                Prototype.Methods.Add(MethodName, method);
+            }
+        }
+    }
+}
