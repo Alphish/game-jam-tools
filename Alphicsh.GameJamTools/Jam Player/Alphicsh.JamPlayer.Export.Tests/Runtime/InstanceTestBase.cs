e6973fa966be110a8fb0b4085aa9f09ded0a26b8
Alice Jankowska <alphish.creature@gmail.com>
2021-11-22 23:59:32 +0100

    [Tests] Created a base for specific instances tests

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export.Tests/Runtime/InstanceTestBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamPlayer.Export.Runtime
+{
+    public class InstanceTestBase<TInstance>
+        where TInstance : IInstance
+    {
+        private TInstance? Instance { get; set; }
+        
+        protected void GivenInstance(TInstance instance)
+            => Instance = instance;
+
+        // -------------
+        // Methods setup
+        // -------------
+
+        private IInstance? InstanceMethod { get; set; }
+        private ICollection<IInstance> MethodArguments { get; } = new List<IInstance>();
+        
+        private IInstance? MethodResult { get; set; }
+        
+        // Givens
+
+        protected void GivenMethodName(CodeName methodName)
+            => InstanceMethod = Instance!.GetMember(methodName);
+        protected void GivenMethodName(string methodName)
+            => GivenMethodName(CodeName.From(methodName));
+
+        protected void GivenNoMethodArguments() { }
+        
+        protected void GivenMethodArgument(IInstance argument)
+            => MethodArguments.Add(argument);
+        
+        // Whens
+
+        protected void WhenMethodExecuted()
+            => MethodResult = InstanceMethod!.Call(MethodArguments);
+        
+        // Thens
+
+        protected IInstance ThenMethodResult()
+            => MethodResult!;
+    }
+}
