1383669fd2fd313f9a881a60f4905cb425ae7fc1
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:24:56 +0100

    [Feature] Created a class for a runtime variable

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/RuntimeVariable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class RuntimeVariable
+    {
+        public VariableDeclaration Declaration { get; }
+        public IInstance Instance { get; }
+
+        public RuntimeVariable(VariableDeclaration declaration, IInstance instance)
+        {
+            if (!instance.Prototype.IsSubtypeOf(declaration.Prototype))
+            {
+                throw new ArgumentException(
+                    $"The value must match the '{declaration.Prototype.Name} {declaration.Name}' variable declaration.",
+                    nameof(instance)
+                    );
+            }
+            
+            Declaration = declaration;
+            Instance = instance;
+        }
+    }
+}
