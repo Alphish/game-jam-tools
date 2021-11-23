9a6d6917b3c3a5b53a6b458791bc429409613b0f
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:37:20 +0100

    [Tests] Created tests for ConstantExpression

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export.Tests/Runtime/Expressions/ConstantExpressionTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class ConstantExpressionTests
+    {
+        [Fact]
+        public void ConstantExpression_CanBeCreatedForDeclaredTypeInstance()
+        {
+            var prototype = SimpleTestPrototype.Lorem;
+            var instance = SimpleTestPrototype.Lorem.CreateInstance();
+            var expression = ConstantExpression.Create(prototype, instance);
+            var variableScope = new VariableScope();
+
+            expression.EvaluatedType.Should().BeSameAs(prototype);
+            expression.Evaluate(variableScope).Should().BeSameAs(instance);
+        }
+        
+        [Fact]
+        public void ConstantExpression_CannotBeCreatedForDifferentTypeInstance()
+        {
+            var prototype = SimpleTestPrototype.Lorem;
+            var instance = SimpleTestPrototype.Ipsum.CreateInstance();
+            Action expressionCreationAttempt = () =>
+            {
+                var expression = ConstantExpression.Create(prototype, instance);
+            };
+
+            expressionCreationAttempt.Should().Throw<ExpressionCompilationException>();
+        }
+        
+        [Fact]
+        public void ConstantExpression_CanBeCreatedForSubtypeOfDeclaredTypeInstance()
+        {
+            var prototype = SimpleTestPrototype.Ipsum;
+            var instance = SimpleTestPrototype.SubIpsum.CreateInstance();
+            var expression = ConstantExpression.Create(prototype, instance);
+            var variableScope = new VariableScope();
+
+            expression.EvaluatedType.Should().BeSameAs(prototype);
+            expression.Evaluate(variableScope).Should().BeSameAs(instance);
+        }
+    }
+}
