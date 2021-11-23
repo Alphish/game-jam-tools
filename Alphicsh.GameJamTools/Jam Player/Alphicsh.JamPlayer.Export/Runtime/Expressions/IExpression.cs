1b357be1157183e93e03008593889c957393c4ae
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:35:21 +0100

    [Feature] Created an interface for an expression that can be evaluated

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/IExpression.cs
@@ -0,0 +1,8 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public interface IExpression
+    {
+        IPrototype EvaluatedType { get; }
+        IInstance Evaluate(IVariableScope scope);
+    }
+}
