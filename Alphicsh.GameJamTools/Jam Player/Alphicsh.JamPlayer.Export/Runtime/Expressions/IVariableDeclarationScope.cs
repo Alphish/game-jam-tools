b1fe4302b365a36bbe08e5bec2292cfae84c01bc
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 20:33:10 +0100

    [Feature] Created a VariableDeclarationScope for checking for variable types and whether given variables exist at all

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/IVariableDeclarationScope.cs
@@ -0,0 +1,11 @@
+﻿using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public interface IVariableDeclarationScope
+    {
+        void DeclareVariable(VariableDeclaration declaration);
+        VariableDeclaration? GetDeclaration(CodeName variableName);
+        IVariableDeclarationScope CreateChildScope();
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/VariableDeclarationScope.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class VariableDeclarationScope : IVariableDeclarationScope
+    {
+        private IVariableDeclarationScope? ParentScope { get; }
+        private IDictionary<CodeName, VariableDeclaration> Declarations { get; }
+
+        public VariableDeclarationScope() : this(parentScope: null) { }
+
+        private VariableDeclarationScope(IVariableDeclarationScope? parentScope)
+        {
+            ParentScope = parentScope;
+            Declarations = new Dictionary<CodeName, VariableDeclaration>();
+        }
+
+        public void DeclareVariable(VariableDeclaration declaration)
+        {
+            Declarations.Add(declaration.Name, declaration);
+        }
+
+        public VariableDeclaration? GetDeclaration(CodeName variableName)
+        {
+            if (Declarations.TryGetValue(variableName, out var ownDeclaration))
+                return ownDeclaration;
+            else if (ParentScope != null)
+                return ParentScope.GetDeclaration(variableName);
+            else
+                return null;
+        }
+
+        public IVariableDeclarationScope CreateChildScope()
+        {
+            return new VariableDeclarationScope(this);
+        }
+    }
+}
