44fae8057cbee7d80f2313be357e8d915c4b33ce
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:36:15 +0100

    [Feature] Created an expression compilation exception for when an exception can't be formed for one reason or another

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/ExpressionCompilationException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class ExpressionCompilationException : Exception
+    {
+        public ExpressionCompilationException(string message)
+            : base(message) { }
+    }
+}
