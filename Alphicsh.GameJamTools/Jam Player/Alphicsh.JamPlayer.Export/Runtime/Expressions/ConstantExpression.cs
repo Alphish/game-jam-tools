64d842c8d2eb5ef14725219974ca1876fa91e6bd
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:36:56 +0100

    [Feature] Created a ConstantExpression to store fixed instances
    Note: the instances themselves might be technically mutable, but oh well.

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/ConstantExpression.cs
@@ -0,0 +1,40 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class ConstantExpression : IExpression
+    {
+        // --------
+        // Creation
+        // --------
+        
+        private IPrototype Prototype { get; }
+        private IInstance Instance { get; }
+
+        private ConstantExpression(IPrototype prototype, IInstance instance)
+        {
+            Prototype = prototype;
+            Instance = instance;
+        }
+
+        public static ConstantExpression Create(IPrototype prototype, IInstance instance)
+        {
+            if (!instance.Prototype.IsSubtypeOf(prototype))
+            {
+                throw new ExpressionCompilationException(
+                    $"The value must match the '{prototype.Name}' variable declaration."
+                );
+            }
+            
+            return new ConstantExpression(prototype, instance);
+        }
+        
+        // ----------
+        // Evaluation
+        // ----------
+
+        public IPrototype EvaluatedType
+            => Prototype;
+
+        public IInstance Evaluate(IVariableScope variableScope)
+            => Instance;
+    }
+}
