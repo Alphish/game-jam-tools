c88aeb811e86abadd64d6d36ba55334d6ba4c496
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:26:05 +0100

    [Feature] Created a variable scope providing instances associated with specific variables

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/IVariableScope.cs
@@ -0,0 +1,11 @@
+﻿using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public interface IVariableScope
+    {
+        void DefineVariable(VariableDeclaration declaration, IInstance instance);
+        IInstance? GetVariableValue(CodeName variableName);
+        IVariableScope CreateChildScope();
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/VariableScope.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Alphicsh.JamPlayer.Export.Runtime.Functions;
+
+namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class VariableScope : IVariableScope
+    {
+        private IVariableScope? ParentScope { get; }
+        private IDictionary<CodeName, RuntimeVariable> VariableValues { get; }
+
+        public VariableScope() : this(parentScope: null) { }
+
+        private VariableScope(IVariableScope? parentScope)
+        {
+            ParentScope = parentScope;
+            VariableValues = new Dictionary<CodeName, RuntimeVariable>();
+        }
+
+        public void DefineVariable(VariableDeclaration declaration, IInstance instance)
+        {
+            var variable = new RuntimeVariable(declaration, instance);
+            VariableValues.Add(declaration.Name, variable);
+        }
+
+        public IInstance? GetVariableValue(CodeName variableName)
+        {
+            if (VariableValues.TryGetValue(variableName, out var ownVariable))
+                return ownVariable.Instance;
+            else if (ParentScope != null)
+                return ParentScope.GetVariableValue(variableName);
+            else
+                return null;
+        }
+
+        public IVariableScope CreateChildScope()
+        {
+            return new VariableScope(this);
+        }
+    }
+}
