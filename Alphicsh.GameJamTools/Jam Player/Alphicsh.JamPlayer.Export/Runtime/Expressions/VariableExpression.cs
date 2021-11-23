646fa68db150f361fbaa803ac7ca88866644854e
Alice Jankowska <alphish.creature@gmail.com>
2021-11-23 22:52:02 +0100

    [Feature] Created a variable expression evaluating to whichever is the current variable value

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Runtime/Expressions/VariableExpression.cs
@@ -0,0 +1,38 @@
+﻿namespace Alphicsh.JamPlayer.Export.Runtime.Expressions
+{
+    public class VariableExpression : IExpression
+    {
+        // --------
+        // Creation
+        // --------
+        
+        private VariableDeclaration Declaration { get; }
+
+        private VariableExpression(VariableDeclaration declaration)
+        {
+            Declaration = declaration;
+        }
+
+        public static VariableExpression Create(IVariableDeclarationScope declarationScope, CodeName variableName)
+        {
+            var declaration = declarationScope.GetDeclaration(variableName);
+            if (declaration == null)
+            {
+                var message = $"Cannot create a variable expression for an unknown variable '{variableName}'.";
+                throw new ExpressionCompilationException(message);
+            }
+
+            return new VariableExpression(declaration);
+        }
+        
+        // ----------
+        // Evaluation
+        // ----------
+
+        public IPrototype EvaluatedType
+            => Declaration.Prototype;
+
+        public IInstance Evaluate(IVariableScope variableScope)
+            => variableScope.GetVariableValue(Declaration.Name)!;
+    }
+}
