d859a1b433c3af41b1821b8d49631cb7d4ae2ce6
Alice Jankowska <alphish.creature@gmail.com>
2021-11-25 20:35:06 +0100

    [Feature] Created a base parsing exception and a specific UnexpectedTokenException for... well, unexpected tokens

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Parser/Parsing/Exceptions/ExportScriptParsingException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Alphicsh.JamPlayer.Export.Parser.Parsing.Exceptions
+{
+    public class ExportScriptParsingException : Exception
+    {
+        public Cursor Start { get; }
+        public Cursor End { get; }
+        
+        public ExportScriptParsingException(string message, Cursor start, Cursor end)
+            : base($"Parsing error at {start.ToShortString()}: " + message)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Parser/Parsing/Exceptions/UnexpectedTokenException.cs
@@ -0,0 +1,45 @@
+﻿using Alphicsh.JamPlayer.Export.Parser.Scanning;
+
+namespace Alphicsh.JamPlayer.Export.Parser.Parsing.Exceptions
+{
+    public class UnexpectedTokenException : ExportScriptParsingException
+    {
+        public ExportScriptTokenType? ExpectedType { get; }
+        public string? ExpectedContent { get; }
+        public ExportScriptToken? ActualToken { get; }
+        
+        public UnexpectedTokenException(
+            ExportScriptTokenType? expectedType,
+            string? expectedContent,
+            ExportScriptToken? actualToken,
+            Cursor start,
+            Cursor end
+            ) : base(BuildMessageForTokens(expectedType, expectedContent, actualToken), start, end)
+        {
+            ExpectedType = expectedType;
+            ExpectedContent = expectedContent;
+            ActualToken = actualToken;
+        }
+
+        private static string BuildMessageForTokens(
+            ExportScriptTokenType? expectedType,
+            string? expectedContent,
+            ExportScriptToken? actualToken
+            )
+        {
+            var actualTokenPart = actualToken != null
+                ? $"Unexpected {actualToken.Type} token with '{actualToken.Content}' content."
+                : $"Unexpected end of tokens.";
+
+            string expectedTokenPart;
+            if (expectedType == null)
+                expectedTokenPart = "End of tokens expected.";
+            else if (expectedContent == null)
+                expectedTokenPart = $"{expectedType} token expected.";
+            else
+                expectedTokenPart = $"{expectedType} token with '{expectedContent}' content expected.";
+
+            return $"{actualTokenPart} {expectedTokenPart}";
+        }
+    }
+}
