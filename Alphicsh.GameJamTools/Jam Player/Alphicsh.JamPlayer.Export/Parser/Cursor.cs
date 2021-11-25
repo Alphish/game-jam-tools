b5d98fba664b1f3c12704d538215be0cdce8c4ac
Alice Jankowska <alphish.creature@gmail.com>
2021-11-25 20:33:26 +0100

    [Feature] Created cursor struct to keep track of position in file

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Export/Parser/Cursor.cs
@@ -0,0 +1,32 @@
+﻿namespace Alphicsh.JamPlayer.Export.Parser
+{
+    public readonly struct Cursor
+    {
+        public int Position { get; } // 0-based
+        public int Line { get; } // 0-based
+        public int Column { get; } // 0-based
+
+        public Cursor(int position, int line, int column)
+        {
+            Position = position;
+            Line = line;
+            Column = column;
+        }
+
+        public Cursor AdvanceColumns(int count)
+            => new Cursor(Position + count, Line, Column + count);
+
+        public Cursor AdvanceLine()
+            => new Cursor(Position + 1, Line + 1, 0);
+
+        public override string ToString()
+        {
+            return $"Line {Line+1}, Column {Column+1}, Position {Position+1}";
+        }
+
+        public string ToShortString()
+        {
+            return $"Line {Line + 1}, Column {Column + 1}";
+        }
+    }
+}
