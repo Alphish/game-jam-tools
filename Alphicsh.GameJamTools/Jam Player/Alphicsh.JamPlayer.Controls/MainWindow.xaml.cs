a90c71783fba90d84b41c62b1587615be5244b0e
Alice Jankowska <alphish.creature@gmail.com>
2021-11-28 14:28:24 +0100

    [Feature] Recreated the MainWindow control in the Alphicsh.JamPlayer.Controls project
    Also, wrapped the ranking tab in a tab.

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Alphicsh.JamPlayer.Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var focusedElement = FocusManager.GetFocusedElement(this);
+            focusedElement?.RaiseEvent(new RoutedEventArgs(UIElement.LostFocusEvent));
+            FocusManager.SetFocusedElement(this, this);
+        }
+    }
+}
