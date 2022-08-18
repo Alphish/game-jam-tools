802bb6613a9c19b7ef71d88dd90c6021c7aa74cd
Alice Jankowska <alphish.creature@gmail.com>
2022-08-19 00:45:57 +0200

    [Feature] Created controls to handle best-of selection
    Also, tweaked the bindings in the MainWindow

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Awards/AwardsTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.Awards
+{
+    /// <summary>
+    /// Interaction logic for AwardsTab.xaml
+    /// </summary>
+    public partial class AwardsTab : UserControl
+    {
+        public AwardsTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
