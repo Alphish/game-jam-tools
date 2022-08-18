a5365b21c6721dc5dbd2ac8ec7a3d60286d97601
Alice Jankowska <alphish.creature@gmail.com>
2022-08-19 00:41:55 +0200

    [Feature] Added view models for the award entities

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Awards/AwardEntryViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Alphicsh.JamPlayer.Model.Awards;
+using Alphicsh.JamPlayer.ViewModel.Jam;
+using Alphicsh.JamTools.Common.Mvvm;
+using Alphicsh.JamTools.Common.Mvvm.NotifiableProperties;
+
+namespace Alphicsh.JamPlayer.ViewModel.Awards
+{
+    public class AwardEntryViewModel : WrapperViewModel<AwardEntry>
+    {
+        public AwardEntryViewModel(AwardEntry model, JamOverviewViewModel jamOverview) : base(model)
+        {
+            Candidates = ListCandidates(jamOverview);
+
+            var currentCandidate = Candidates.Single(candidate => candidate.Entry?.Model.Id == model.JamEntry?.Id);
+            EntryProperty = MutableProperty.Create(this, nameof(Entry), currentCandidate);
+        }
+
+        public string Description => Model.Criterion.Description;
+
+        public MutableProperty<AwardCandidateViewModel> EntryProperty { get; }
+        public AwardCandidateViewModel Entry
+        {
+            get => EntryProperty.Value;
+            set
+            {
+                Model.JamEntry = value.Entry?.Model;
+                EntryProperty.Value = value;
+            }
+        }
+
+        public IReadOnlyCollection<AwardCandidateViewModel> Candidates { get; set; }
+
+        private IReadOnlyCollection<AwardCandidateViewModel> ListCandidates(JamOverviewViewModel jamOverview)
+        {
+            var candidates = jamOverview.Entries
+                .OrderBy(entry => entry.Title)
+                .ThenBy(entry => entry.Team.ShortDescription)
+                .Select(entry => new AwardCandidateViewModel { Entry = entry })
+                .ToList();
+            candidates.Insert(0, AwardCandidateViewModel.Empty);
+
+            return candidates;
+        }
+    }
+}
