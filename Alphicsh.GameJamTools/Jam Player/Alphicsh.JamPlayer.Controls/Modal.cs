f299708d3cd2fc18a7c89ac1466da79c2b81067f
Alice Jankowska <alphish.creature@gmail.com>
2022-08-25 02:20:38 +0200

    [Feature] Added a general method for opening the modal windows

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Modal.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Alphicsh.JamPlayer.ViewModel;
+using Alphicsh.JamTools.Common.Mvvm.Modals;
+
+namespace Alphicsh.JamPlayer.Controls
+{
+    public static class Modal
+    {
+        public static void Show(Window modal, ModalViewModel viewModel)
+        {
+            AppViewModel.Current.HasOverlay = true;
+            modal.Owner = MainWindow.Current;
+            modal.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            modal.DataContext = viewModel;
+            modal.ShowDialog();
+            AppViewModel.Current.HasOverlay = false;
+        }
+    }
+}
