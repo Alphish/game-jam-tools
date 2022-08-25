ec4e47879adeb77a9f26ffb1e9ca6f2da5bda58b
Alice Jankowska <alphish.creature@gmail.com>
2022-08-25 02:22:20 +0200

    [Feature] Created the ExportHelpModal for showing export settings reference

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Export/Modals/ExportHelpViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using Alphicsh.JamTools.Common.Mvvm.Modals;
+
+namespace Alphicsh.JamPlayer.ViewModel.Export.Modals
+{
+    public class ExportHelpViewModel : ModalViewModel
+    {
+        public ExportHelpViewModel(Window window)
+            : base(window, "Export options reference")
+        {
+        }
+    }
+}
