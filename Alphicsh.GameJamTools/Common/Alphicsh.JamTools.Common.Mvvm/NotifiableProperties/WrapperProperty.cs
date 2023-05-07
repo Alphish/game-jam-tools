f979c0916da6895d9ed4462442a2ef63332de6a7
Alice Jankowska <alphish.creature@gmail.com>
2023-05-07 20:37:16 +0200

    [Refactor] Added expression-based constructor for WrapperProperty

--- Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.Mvvm/NotifiableProperties/WrapperProperty.cs
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.Mvvm/NotifiableProperties/WrapperProperty.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Alphicsh.JamTools.Common.Mvvm.NotifiableProperties
 {
@@ -61,6 +63,30 @@ namespace Alphicsh.JamTools.Common.Mvvm.NotifiableProperties
             where TViewModel : IViewModel
         {
             return new WrapperProperty<TViewModel, TValue>(viewModel, propertyName, valueGetter, valueSetter: null);
+        }
+
+        public static WrapperProperty<TViewModel, TValue> Create<TViewModel, TValue>(
+            TViewModel viewModel, string propertyName, Expression<Func<TViewModel, TValue>> propertyExpression
+            )
+            where TViewModel : IViewModel
+        {
+            var valueGetter = propertyExpression.Compile();
+            var valueSetter = CreateSetterFromGetter(propertyExpression);
+            return new WrapperProperty<TViewModel, TValue>(viewModel, propertyName, valueGetter, valueSetter);
+        }
+
+        private static Action<TViewModel, TValue> CreateSetterFromGetter<TViewModel, TValue>(
+            Expression<Func<TViewModel, TValue>> getterExpression
+            )
+        {
+            var viewModelParameter = getterExpression.Parameters.First();
+            var valueGetterBody = getterExpression.Body;
+            var valueSetterValue = Expression.Parameter(typeof(TValue), "value");
+            var valueSetterBody = Expression.Assign(valueGetterBody, valueSetterValue);
+            var valueSetterExpression = Expression.Lambda<Action<TViewModel, TValue>>(
+                valueSetterBody, viewModelParameter, valueSetterValue
+                );
+            return valueSetterExpression.Compile();
         }
     }
 }
