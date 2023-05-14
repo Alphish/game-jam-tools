59ffada021d99b34d1a03045ecc7ff745efbce6e
Alice Jankowska <alphish.creature@gmail.com>
2023-05-14 23:03:15 +0200

    [Feature] Added opening Jam entry from the first command line argument passed to the Entry Packager

--- Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.App/App.xaml.cs
+++ Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.App/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using Alphicsh.EntryPackager.Controls;
 using Alphicsh.EntryPackager.Model;
 using Alphicsh.EntryPackager.ViewModel;
+using Alphicsh.JamTools.Common.IO;
 using Alphicsh.JamTools.Common.Theming;
 
 namespace Alphicsh.EntryPackager.App
@@ -24,11 +26,21 @@ namespace Alphicsh.EntryPackager.App
         protected override void OnStartup(StartupEventArgs e)
         {
             ThemeManager.Create(Resources);
+            OpenEntryFromArgs(e);
 
             base.OnStartup(e);
 
             var window = new MainWindow() { DataContext = ViewModel };
             window.Show();
+        }
+
+        private void OpenEntryFromArgs(StartupEventArgs e)
+        {
+            var directoryPath = FilePath.FromNullable(e.Args.FirstOrDefault());
+            if (directoryPath.HasValue && directoryPath.Value.HasDirectory())
+            {
+                ViewModel.LoadEntryDirectory(directoryPath.Value);
+            }
         }
     }
 }
