aeca50d1b8790f44cecca5be88078fd26209fe97
Alice Jankowska <alphish.creature@gmail.com>
2023-05-24 22:45:05 +0200

    [Feature] Added preview of the entry as it would appear in the ranking list

---
+++ Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.Controls/Preview/EntryPreviewInRankingSection.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.EntryPackager.Controls.Preview
+{
+    /// <summary>
+    /// Interaction logic for EntryPreviewInRankingSection.xaml
+    /// </summary>
+    public partial class EntryPreviewInRankingSection : UserControl
+    {
+        public EntryPreviewInRankingSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
