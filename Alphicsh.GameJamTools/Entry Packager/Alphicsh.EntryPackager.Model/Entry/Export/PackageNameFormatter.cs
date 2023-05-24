82714fcc8416995b7056537ae01e563035b982e4
Alice Jankowska <alphish.creature@gmail.com>
2023-05-24 22:20:13 +0200

    [Refactor] Moved and expanded package name formatting

---
+++ Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.Model/Entry/Export/PackageNameFormatter.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Alphicsh.EntryPackager.Model.Entry.Export
+{
+    public class PackageNameFormatter
+    {
+        public string GetPackageName(string title, string team)
+        {
+            var sanitizedTitle = Sanitize(title);
+            if (string.IsNullOrWhiteSpace(sanitizedTitle))
+                sanitizedTitle = "Entry"; // handling cases when title has no ASCII characters whatsoever
+
+            var sanitizedTeam = Sanitize(team);
+            if (string.IsNullOrWhiteSpace(sanitizedTeam))
+                sanitizedTeam = "Team"; // same, but for the team name
+
+            return sanitizedTitle + " by " + sanitizedTeam;
+        }
+
+        private string Sanitize(string text)
+        {
+            text = text.Trim();
+            text = Substitute(text);
+            text = FilterInvalidCharacters(text);
+            if (text.Length > 40)
+                text = text.Substring(0, 40);
+            
+            return text;
+        }
+
+        // ------------
+        // Latinization
+        // ------------
+
+        private static Dictionary<char, string> DirectMappings { get; } = new Dictionary<char, string>()
+        {
+            [':'] = " -",
+            ['Ł'] = "L",
+            ['ł'] = "l",
+        };
+
+        private string Substitute(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString.ToCharArray())
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory == UnicodeCategory.NonSpacingMark)
+                    continue;
+
+                if (DirectMappings.ContainsKey(c))
+                    stringBuilder.Append(DirectMappings[c]);
+                else
+                    stringBuilder.Append(c);
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+
+        // ---------
+        // Filtering
+        // ---------
+
+        private string FilterInvalidCharacters(string text)
+        {
+            return Regex.Replace(text, "[^0-9A-Za-z '.,!()_-]", "");
+        }
+    }
+}
--- Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.Model/Entry/JamEntryEditable.cs
+++ Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.Model/Entry/JamEntryEditable.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using Alphicsh.EntryPackager.Model.Entry.Files;
+﻿using Alphicsh.EntryPackager.Model.Entry.Files;
 
 namespace Alphicsh.EntryPackager.Model.Entry
 {
@@ -12,15 +10,5 @@ namespace Alphicsh.EntryPackager.Model.Entry
 
         public JamTeamEditable Team { get; } = new JamTeamEditable();
         public JamFilesEditable Files { get; } = new JamFilesEditable();
-
-        public string GetDefaultPackageName()
-        {
-            var title = !string.IsNullOrEmpty(Title) ? Title : "Title";
-            var authors = !string.IsNullOrEmpty(Team.DisplayName) ? Team.DisplayName : "Authors";
-            var directoryName = title + " by " + authors;
-            var sanitizedName = directoryName.Replace(":", " -");
-            sanitizedName = Regex.Replace(sanitizedName, "[^0-9A-Za-z '!,()_-]", "");
-            return sanitizedName.Substring(0, Math.Min(sanitizedName.Length, 80));
-        }
     }
 }
