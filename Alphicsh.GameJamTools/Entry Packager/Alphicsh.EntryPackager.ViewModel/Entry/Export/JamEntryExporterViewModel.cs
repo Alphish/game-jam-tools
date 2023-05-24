53fb61e2651931957e05ed54c11490028b288d35
Alice Jankowska <alphish.creature@gmail.com>
2023-05-24 22:26:34 +0200

    [Feature] Implemented the export functionality

--- Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.ViewModel/Entry/Export/JamEntryExporterViewModel.cs
+++ Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.ViewModel/Entry/Export/JamEntryExporterViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Alphicsh.EntryPackager.Model.Entry.Export;
+using Alphicsh.JamTools.Common.Controls.Files;
 using Alphicsh.JamTools.Common.Mvvm;
+using Alphicsh.JamTools.Common.Mvvm.Commands;
+using Alphicsh.JamTools.Common.Mvvm.NotifiableProperties;
 
 namespace Alphicsh.EntryPackager.ViewModel.Entry.Export
 {
@@ -8,9 +11,35 @@ namespace Alphicsh.EntryPackager.ViewModel.Entry.Export
         public JamEntryExporterViewModel(JamEntryExporter model) : base(model)
         {
             Checklist = new JamEntryChecklistViewModel(model.Checklist);
+
+            ExportCommand = ConditionalCommand.From(CanExport, Export);
+            foreach (var statusItem in Checklist.Statuses)
+            {
+                ExportCommand.ExecutionDependingOn(statusItem.StatusIconProperty);
+            }
+            foreach (var reminder in Checklist.Reminders)
+            {
+                ExportCommand.ExecutionDependingOn(reminder.IsCheckedProperty);
+            }
         }
 
         public JamEntryChecklistViewModel Checklist { get; }
+
+        public IConditionalCommand ExportCommand { get; }
+        private bool CanExport() => Model.CanExport();
+        private void Export()
+        {
+            var defaultName = Model.GetDefaultPackageName();
+            var zipPath = FileQuery.SaveFile()
+                .FromDirectory(Model.EntryData.Files.DirectoryPath.GetParentDirectoryPath()!.Value)
+                .WithFileType("*.zip", "ZIP archive")
+                .WithDefaultName(defaultName)
+                .GetPath();
+
+            if (zipPath == null)
+                return;
+
+            Model.ExportTo(zipPath.Value);
         }
     }
 }
