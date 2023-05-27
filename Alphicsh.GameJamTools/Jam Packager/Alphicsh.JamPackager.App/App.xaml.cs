e34b42911044752b919611caeec5632c7cd322c0
Alice Jankowska <alphish.creature@gmail.com>
2023-05-27 23:52:42 +0200

    [Setup] Moved the Jam Packager main window to the new JamPackager.Controls project

--- Alphicsh.GameJamTools/Jam Packager/Alphicsh.JamPackager.App/App.xaml.cs
+++ Alphicsh.GameJamTools/Jam Packager/Alphicsh.JamPackager.App/App.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using Alphicsh.JamPackager.Model;
+using Alphicsh.JamPackager.ViewModel;
+using Alphicsh.JamPackager.Controls;
+using Alphicsh.JamTools.Common.Theming;
 
 namespace Alphicsh.JamPackager.App
 {
@@ -7,5 +11,22 @@ namespace Alphicsh.JamPackager.App
     /// </summary>
     public partial class App : Application
     {
+        public JamPackagerViewModel ViewModel { get; }
+
+        public App()
+        {
+            var model = new JamPackagerModel();
+            ViewModel = new JamPackagerViewModel(model);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ThemeManager.Create(Resources);
+
+            base.OnStartup(e);
+
+            var window = new MainWindow() { DataContext = ViewModel };
+            window.Show();
+        }
     }
 }
---
+++ Alphicsh.GameJamTools/Jam Packager/Alphicsh.JamPackager.Controls/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Alphicsh.JamTools.Common.Mvvm;
+
+namespace Alphicsh.JamPackager.Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : AppWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
