f12d98d95fb15d8a13012bc3a1f21d98d0569f7b
Alice Jankowska <alphish.creature@gmail.com>
2023-06-06 19:58:29 +0200

    [Refactor] Adapted the view models to the new models structure

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Jam/Files/JamAfterwordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Alphicsh.JamPlayer.Model.Jam.Files;
+using Alphicsh.JamTools.Common.IO.Execution;
+using Alphicsh.JamTools.Common.Mvvm.Commands;
+using System.Windows.Input;
+using Alphicsh.JamTools.Common.Mvvm;
+
+namespace Alphicsh.JamPlayer.ViewModel.Jam.Files
+{
+    public class JamAfterwordViewModel : WrapperViewModel<JamAfterword>
+    {
+        private static ProcessLauncher ProcessLauncher { get; } = new ProcessLauncher();
+
+        public JamAfterwordViewModel(JamAfterword model) : base(model)
+        {
+            OpenAfterwordCommand = SimpleCommand.From(OpenAfterword);
+        }
+
+        public ICommand OpenAfterwordCommand { get; }
+        private void OpenAfterword()
+        {
+            ProcessLauncher.OpenFile(Model.Path);
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Jam/Files/JamThumbnailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Media;
+using Alphicsh.JamPlayer.Model.Jam.Files;
+using Alphicsh.JamTools.Common.Mvvm;
+using Alphicsh.JamTools.Common.Mvvm.NotifiableProperties;
+
+namespace Alphicsh.JamPlayer.ViewModel.Jam.Files
+{
+    public class JamThumbnailsViewModel : WrapperViewModel<JamThumbnails>
+    {
+        public JamThumbnailsViewModel(JamThumbnails model) : base(model)
+        {
+            ThumbnailProperty = ImageSourceProperty.CreateReadonly(this, nameof(Thumbnail), vm => vm.Model.ThumbnailPath);
+            ThumbnailSmallProperty = ImageSourceProperty.CreateReadonly(this, nameof(ThumbnailSmall), vm => vm.Model.ThumbnailSmallPath);
+        }
+
+        // ------
+        // Images
+        // ------
+
+        public ImageSourceProperty<JamThumbnailsViewModel> ThumbnailProperty { get; }
+        public ImageSource? Thumbnail => ThumbnailProperty.ImageSource ?? ThumbnailSmallProperty.ImageSource;
+
+        public ImageSourceProperty<JamThumbnailsViewModel> ThumbnailSmallProperty { get; }
+        public ImageSource? ThumbnailSmall => ThumbnailSmallProperty.ImageSource ?? ThumbnailProperty.ImageSource;
+    }
+}
