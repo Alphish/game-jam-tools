23ef4d9ba8c814d327673cc700b53bac5fc2c523
Alice Jankowska <alphish.creature@gmail.com>
2023-06-22 01:56:55 +0200

    [Feature] Added a model for managing jam vote collections

---
+++ Alphicsh.GameJamTools/Jam Tally/Alphicsh.JamTally.Model/Vote/JamVoteCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Alphicsh.JamTools.Common.IO;
+
+namespace Alphicsh.JamTally.Model.Vote
+{
+    public class JamVoteCollection
+    {
+        private static JamVoteSaver VoteSaver { get; } = new JamVoteSaver();
+
+        public FilePath DirectoryPath { get; init; } = default!;
+        public IList<JamVote> Votes { get; init; } = default!;
+
+        public void AddVote()
+        {
+            Votes.Add(new JamVote());
+        }
+
+        public void RemoveVote(JamVote vote)
+        {
+            Votes.Remove(vote);
+        }
+
+        public void SaveVotes()
+        {
+            VoteSaver.SaveToDirectory(DirectoryPath, this);
+        }
+    }
+}
