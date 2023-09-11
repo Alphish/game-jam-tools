60372753e24b478e70442eb49cd498c8211c3a6d
Alice Jankowska <alphish.creature@gmail.com>
2023-09-11 14:40:04 +0200

    ISS-5: Added controls for the entry searching modals

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Controls/Ranking/Modals/FoundEntriesListBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Alphicsh.JamPlayer.Controls.Ranking.Modals
+{
+    /// <summary>
+    /// Interaction logic for FoundEntriesListBox.xaml
+    /// </summary>
+    public partial class FoundEntriesListBox : UserControl
+    {
+        public FoundEntriesListBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
