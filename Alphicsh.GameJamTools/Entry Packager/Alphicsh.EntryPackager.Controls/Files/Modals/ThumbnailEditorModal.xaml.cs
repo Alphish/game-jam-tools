bddfcb04d86207dfe8525f305a3e2f3556bd322f
Alice Jankowska <alphish.creature@gmail.com>
2024-02-22 21:50:43 +0100

    ISS-8: Created the controls for the thumbnail editor window

---
+++ Alphicsh.GameJamTools/Entry Packager/Alphicsh.EntryPackager.Controls/Files/Modals/ThumbnailEditorModal.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Alphicsh.EntryPackager.Controls.Files.Modals
+{
+    /// <summary>
+    /// Interaction logic for ThumbnailEditorModal.xaml
+    /// </summary>
+    public partial class ThumbnailEditorModal : Window
+    {
+        public ThumbnailEditorModal()
+        {
+            InitializeComponent();
+        }
+    }
+}
