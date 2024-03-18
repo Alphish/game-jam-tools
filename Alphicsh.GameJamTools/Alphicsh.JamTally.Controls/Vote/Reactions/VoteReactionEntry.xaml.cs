19615b2353b82a8ad1429afcff4e6673c3536d4a
Alice Jankowska <alphish.creature@gmail.com>
2024-03-18 19:47:47 +0100

    ISS-10: Added controls for vote reactions section

---
+++ Alphicsh.GameJamTools/Alphicsh.JamTally.Controls/Vote/Reactions/VoteReactionEntry.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Alphicsh.JamTally.Controls.Vote.Reactions
+{
+    /// <summary>
+    /// Interaction logic for VoteReactionEntry.xaml
+    /// </summary>
+    public partial class VoteReactionEntry : UserControl
+    {
+        public VoteReactionEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Alphicsh.JamTally.Controls/Vote/Reactions/VoteReactionsSection.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Alphicsh.JamTally.Controls.Vote.Reactions
+{
+    /// <summary>
+    /// Interaction logic for VoteReactionsSection.xaml
+    /// </summary>
+    public partial class VoteReactionsSection : UserControl
+    {
+        public VoteReactionsSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
