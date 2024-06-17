fff60831e1feae34afe6293da1b061f4f3b18922
Alice Jankowska <alphish.creature@gmail.com>
2024-06-18 00:04:21 +0200

    ISS-12: Created a JSON formatter within the new namespace

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Storage/Formats/BaseFormatter.cs
@@ -0,0 +1,21 @@
+﻿namespace Alphicsh.JamTools.Common.IO.Storage.Formats
+{
+    public abstract class BaseFormatter<TEntity> : IFormatter<TEntity>
+        where TEntity : class
+    {
+        public abstract string Format(TEntity entity);
+
+        public FileData FormatToFile(FilePath path, TEntity entity)
+        {
+            var content = Format(entity);
+            return new FileData(path, content);
+        }
+
+        public abstract TEntity? Parse(string content);
+
+        public TEntity? ParseFromFile(FileData file)
+        {
+            return Parse(file.Content);
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Storage/Formats/JsonFormatter.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Alphicsh.JamTools.Common.IO.Serialization;
+
+namespace Alphicsh.JamTools.Common.IO.Storage.Formats
+{
+    public class JsonFormatter<TEntity> : BaseFormatter<TEntity>
+        where TEntity : class
+    {
+        // -----
+        // Setup
+        // -----
+
+        protected JsonSerializerOptions SerializerOptions { get; }
+
+        public JsonFormatter() : this(JsonIgnoreCondition.WhenWritingNull)
+        {
+        }
+
+        public JsonFormatter(JsonIgnoreCondition ignoreCondition)
+        {
+            SerializerOptions = new JsonSerializerOptions()
+            {
+                // characters encoding
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+
+                // allowing flexible JSONs
+                AllowTrailingCommas = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+
+                // ignoring properties
+                DefaultIgnoreCondition = ignoreCondition,
+                IncludeFields = false,
+
+                // property names
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+
+                // indentation
+                WriteIndented = true,
+            };
+
+            // converters
+            SerializerOptions.Converters.Add(new FilePathJsonConverter());
+        }
+
+        // --------------
+        // Implementation
+        // --------------
+
+        public override string Format(TEntity entity)
+        {
+            return JsonSerializer.Serialize(entity, SerializerOptions);
+        }
+
+        public override TEntity? Parse(string content)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<TEntity>(content, SerializerOptions);
+            }
+            catch (JsonException)
+            {
+                return null;
+            }
+        }
+    }
+}
