9a2eb33cd48e420501e7ae7ecdc664a845d29df6
Alice Jankowska <alphish.creature@gmail.com>
2024-09-01 01:02:31 +0200

    ISS-14: Added Best Reviewer property to the view model and awards tab

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Awards/AwardsOverviewViewModel.cs
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.ViewModel/Awards/AwardsOverviewViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Alphicsh.JamPlayer.Model.Awards;
 using Alphicsh.JamPlayer.ViewModel.Jam;
 using Alphicsh.JamTools.Common.Mvvm;
+using Alphicsh.JamTools.Common.Mvvm.NotifiableProperties;
 
 namespace Alphicsh.JamPlayer.ViewModel.Awards
 {
@@ -12,8 +13,13 @@ namespace Alphicsh.JamPlayer.ViewModel.Awards
         public AwardsOverviewViewModel(AwardsOverview model, JamOverviewViewModel jamOverview) : base(model)
         {
             Entries = model.Entries.Select(entry => new AwardEntryViewModel(entry, jamOverview)).ToList();
+
+            BestReviewerProperty = WrapperProperty.ForMember(this, vm => vm.Model.BestReviewer);
         }
 
         public IReadOnlyCollection<AwardEntryViewModel> Entries { get; set; }
+
+        public WrapperProperty<AwardsOverviewViewModel, string?> BestReviewerProperty { get; }
+        public string? BestReviewer { get => BestReviewerProperty.Value; set => BestReviewerProperty.Value = value; }
     }
 }
