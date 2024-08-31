1827d0dc22adb5c946931cc2a07f2e9c00a0d398
Alice Jankowska <alphish.creature@gmail.com>
2024-08-31 23:11:21 +0200

    ISS-12: Added a Jam feedback model combining the ranking and awards models

---
+++ Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.Model/Feedback/JamFeedback.cs
@@ -0,0 +1,14 @@
+﻿using Alphicsh.JamPlayer.Model.Awards;
+using Alphicsh.JamPlayer.Model.Ranking;
+using Alphicsh.JamTools.Common.IO;
+
+namespace Alphicsh.JamPlayer.Model.Feedback
+{
+    public class JamFeedback
+    {
+        public FilePath Location { get; internal set; } = default!;
+
+        public RankingOverview Ranking { get; internal set; } = new RankingOverview();
+        public AwardsOverview Awards { get; internal set; } = new AwardsOverview();
+    }
+}
