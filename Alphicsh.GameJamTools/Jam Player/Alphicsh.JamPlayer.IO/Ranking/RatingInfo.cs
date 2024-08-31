c0f15965bb9110b3c354b2f0e20b051d0dec4b06
Alice Jankowska <alphish.creature@gmail.com>
2024-08-31 23:39:53 +0200

    ISS-12: Removed old feedback information structures and serialisers

--- Alphicsh.GameJamTools/Jam Player/Alphicsh.JamPlayer.IO/Ranking/RatingInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Alphicsh.JamPlayer.IO.Ranking
-{
-    public class RatingInfo
-    {
-        public string Id { get; init; } = default!;
-        public object? Value { get; init; } = default!;
-    }
-}
