f5b5eab48f7577dd44fba64d044b7461fc777524
Alice Jankowska <alphish.creature@gmail.com>
2024-08-31 22:59:00 +0200

    ISS-12: Added model saver interfaces and base classes

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Storage/Saving/BaseModelSaver.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace Alphicsh.JamTools.Common.IO.Storage.Saving
+{
+    public class BaseModelSaver<TModel, TInfo> : IModelSaver<TModel>
+        where TModel : class
+        where TInfo : class
+    {
+        private static FileDataManager FileDataManager { get; } = new FileDataManager();
+
+        private IMapper<TModel, TInfo> ModelMapper { get; }
+        private IModelInfoWriter<TInfo> InfoWriter { get; }
+
+        public BaseModelSaver(IMapper<TModel, TInfo> modelMapper, IModelInfoWriter<TInfo> infoWriter)
+        {
+            ModelMapper = modelMapper;
+            InfoWriter = infoWriter;
+        }
+
+        public FileBatch PrepareFileBatch(TModel model)
+        {
+            var modelInfo = ModelMapper.Map(model);
+            return InfoWriter.SerializeModelInfo(modelInfo);
+        }
+
+        public Task<FileBatch> PrepareFileBatchAsync(TModel model)
+        {
+            return Task.Run(() => PrepareFileBatch(model));
+        }
+
+        public async Task SaveFileBatch(FileBatch newBatch, FileBatch previousBatch)
+        {
+            await FileDataManager.WriteData(newBatch, previousBatch);
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Storage/Saving/IModelSaver.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Alphicsh.JamTools.Common.IO.Storage.Saving
+{
+    public interface IModelSaver<TModel> where TModel : class
+    {
+        FileBatch PrepareFileBatch(TModel model);
+        Task<FileBatch> PrepareFileBatchAsync(TModel model);
+
+        Task SaveFileBatch(FileBatch newBatch, FileBatch previousBatch);
+    }
+}
