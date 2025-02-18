96dc2203b6e4c7dbfc0abf7fc7c2e49cd830b8f8
Alice Jankowska <alphish.creature@gmail.com>
2025-02-18 20:24:48 +0100

    ISS-17: Added information about entries and their individual trophies

---
+++ Alphicsh.GameJamTools/Jam Tally/Alphicsh.JamTally.Model/Result/Trophies/Data/JamTrophyEntry.cs
@@ -0,0 +1,21 @@
+﻿using Alphicsh.JamTally.Model.Jam;
+
+namespace Alphicsh.JamTally.Model.Result.Trophies.Data
+{
+    public class JamTrophyEntry
+    {
+        public JamEntry Entry { get; }
+
+        public JamTrophyEntry(JamEntry entry)
+        {
+            Entry = entry;
+            Id = string.Empty;
+            Title = entry.FullTitle;
+            Authors = entry.Team;
+        }
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+    }
+}
