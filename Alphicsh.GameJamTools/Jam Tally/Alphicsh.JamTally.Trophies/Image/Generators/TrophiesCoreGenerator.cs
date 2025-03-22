d85bcd2857c237fe2f55914c91806b2333b45f37
Alice Jankowska <alphish.creature@gmail.com>
2025-03-22 20:36:24 +0100

    ISS-18: Implemented a generator for trophies core

---
+++ Alphicsh.GameJamTools/Jam Tally/Alphicsh.JamTally.Trophies/Image/Generators/TrophiesCoreGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace Alphicsh.JamTally.Trophies.Image.Generators
+{
+    public class TrophiesCoreGenerator
+    {
+        public TrophiesImage Generate(TrophiesCoreSettings coreSettings)
+        {
+            var image = TrophiesImage.CreateBlank(coreSettings.TrophyWidth, coreSettings.TrophyHeight, coreSettings.ColumnWidth, coreSettings.RowHeight)
+                .WithLayer("medals_back", "Medals Back")
+                .WithLayer("medals_rim", "Medals Rim")
+                .WithLayer("medals_sheen", "Medals Sheen")
+                .WithLayer("text_stroke", "Text Stroke")
+                .WithLayer("text_fill", "Text Fill")
+                .WithMedalSection("gold", row: -5, column: 0)
+                .WithMedalSection("silver", row: -4, column: 0)
+                .WithMedalSection("bronze", row: -3, column: 0)
+                .WithMedalSection("award", row: -2, column: 0)
+                .WithMedalSection("basic", row: -1, column: 0);
+
+            return image;
+        }
+    }
+}
---
+++ Alphicsh.GameJamTools/Jam Tally/Alphicsh.JamTally.Trophies/Image/Generators/TrophiesCoreSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Alphicsh.JamTally.Trophies.Image.Generators
+{
+    public class TrophiesCoreSettings
+    {
+        public int TrophyWidth { get; init; }
+        public int TrophyHeight { get; init; }
+        public int ColumnWidth { get; init; }
+        public int RowHeight { get; init; }
+    }
+}
