c317c48ed6ce15eea705247d027ae13d4f992808
Alice Jankowska <alphish.creature@gmail.com>
2025-04-24 20:24:38 +0200

    ISS-25: Implemented common view models for the checklist functionality

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.ViewModel/Checklists/CheckConfirmationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Alphicsh.JamTools.Common.Checklists;
+using Alphicsh.JamTools.Common.Mvvm;
+using Alphicsh.JamTools.Common.Mvvm.NotifiableProperties;
+
+namespace Alphicsh.JamTools.Common.ViewModel.Checklists
+{
+    public class CheckConfirmationViewModel : WrapperViewModel<CheckConfirmation>
+    {
+        public CheckConfirmationViewModel(CheckConfirmation model) : base(model)
+        {
+            Description = model.Description;
+
+            IsConfirmedProperty = WrapperProperty.ForMember(this, vm => vm.Model.IsConfirmed);
+        }
+
+        public string Description { get; }
+
+        public WrapperProperty<CheckConfirmationViewModel, bool> IsConfirmedProperty { get; }
+        public bool IsConfirmed { get => IsConfirmedProperty.Value; set => IsConfirmedProperty.Value = value; }
+    }
+}
