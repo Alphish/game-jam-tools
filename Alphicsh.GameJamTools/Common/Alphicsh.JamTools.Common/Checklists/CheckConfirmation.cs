97fa6fb4a7786888485757db2db1fe640559fc0f
Alice Jankowska <alphish.creature@gmail.com>
2025-04-24 20:23:22 +0200

    ISS-25: Implemented a basic checklist model

---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common/Checklists/CheckConfirmation.cs
@@ -0,0 +1,9 @@
+﻿namespace Alphicsh.JamTools.Common.Checklists
+{
+    public class CheckConfirmation
+    {
+        public bool IsConfirmed { get; set; } = false;
+        public bool IsMandatory { get; init; } = true;
+        public string Description { get; init; } = default!;
+    }
+}
---
+++ Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common/Checklists/ChecklistStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Alphicsh.JamTools.Common.Checklists
+{
+    public enum ChecklistStatus
+    {
+        Present = 1,
+        Absent = 2,
+        Invalid = 3,
+    }
+}
