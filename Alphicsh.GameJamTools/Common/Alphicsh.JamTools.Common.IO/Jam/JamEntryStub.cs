fcd6ff6e5e316c8c9e48ad92e740e1f2d4e93238
Alice Jankowska <alphish.creature@gmail.com>
2025-08-14 20:32:08 +0200

    ISS-29: Removed old classes specific to the jam itself (no entry yet)

--- Alphicsh.GameJamTools/Common/Alphicsh.JamTools.Common.IO/Jam/JamEntryStub.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Alphicsh.JamTools.Common.IO.Jam
-{
-    public class JamEntryStub
-    {
-        public string Id { get; init; } = default!;
-        public string EntrySubpath { get; init; } = default!;
-    }
-}
