079d4cfa9fdd34f4d2b6868d276fad0936c98c85
Alice Jankowska <alphish.creature@gmail.com>
2025-08-20 22:20:42 +0200

    ISS-30: Expanded the jam view model to handle new fields

---
+++ Alphicsh.GameJamTools/Jam Packager/Alphicsh.JamPackager.ViewModel/Jam/JamLinkEditableViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Alphicsh.JamPackager.Model.Jam;
+using Alphicsh.JamTools.Common.Mvvm;
+using Alphicsh.JamTools.Common.Mvvm.NotifiableProperties;
+
+namespace Alphicsh.JamPackager.ViewModel.Jam
+{
+    public class JamLinkEditableViewModel : WrapperViewModel<JamLinkEditable>
+    {
+        public static CollectionViewModelStub<JamLinkEditable, JamLinkEditableViewModel> CollectionStub
+            => CollectionViewModelStub.Create((JamLinkEditable model) => new JamLinkEditableViewModel(model));
+
+        public JamLinkEditableViewModel(JamLinkEditable model) : base(model)
+        {
+            TitleProperty = WrapperProperty.ForMember(this, vm => vm.Model.Title);
+            UrlProperty = WrapperProperty.ForMember(this, vm => vm.Model.Url);
+        }
+
+        public WrapperProperty<JamLinkEditableViewModel, string> TitleProperty { get; }
+        public string Title { get => TitleProperty.Value; set => TitleProperty.Value = value; }
+
+        public WrapperProperty<JamLinkEditableViewModel, string> UrlProperty { get; }
+        public string Url { get => UrlProperty.Value; set => UrlProperty.Value = value; }
+    }
+}
