282f6c99e35466e38e66f0eab3d715a9b75cb457
Alice Jankowska <alphish.creature@gmail.com>
2025-09-03 08:24:35 +0200

    ISS-32: Implemented structures and a tool for exporting tallied results to easily processed JSON

---
+++ Alphicsh.GameJamTools/Jam Tally/Alphicsh.JamTally.Model/Result/Export/JamTallyResultInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Alphicsh.JamTally.Model.Result.Export
+{
+    public class JamTallyResultInfo
+    {
+        public IReadOnlyCollection<string> Ranking { get; init; } = default!;
+        public IReadOnlyCollection<JamTallyAwardInfo> Awards { get; init; } = default!;
+    }
+}
